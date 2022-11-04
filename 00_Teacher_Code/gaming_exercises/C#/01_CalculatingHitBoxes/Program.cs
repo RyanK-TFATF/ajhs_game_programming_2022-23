d91f3e00ba41c1bc62eddfbf9f8730ec4503bbec
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-04 12:59:54 -0400

    v0.2a
    Adjusted variable names for clarity.

--- 00_Teacher_Code/gaming_exercises/C#/01_CalculatingHitBoxes/Program.cs
+++ 00_Teacher_Code/gaming_exercises/C#/01_CalculatingHitBoxes/Program.cs
@@ -1,4 +1,4 @@
-﻿// Calculating Hit Boxes, Ryan Kelley, 11/04/22, 12:26PM, v0.2
+﻿// Calculating Hit Boxes, Ryan Kelley, 11/04/22, 12:26PM, v0.2a
 
 using System;
 
@@ -11,27 +11,25 @@ namespace _01_CalculatingHitBoxes
             // Variable Declarataions       
             int hitBoxType = 0; // 1 = 2D hit box, 2 = 3D hit box
 
-            // 2D Hit Box Measurements 
-            int hitBox2DLengthA = 0; 
-            int hitBox2DWidthA = 0; 
-            int hitBox2DLengthB = 0; 
-            int hitBox2DWidthB = 0; 
-
-            // 2D Hit Box Calculations
-            int area2DHitBoxA = hitBox2DLengthA * hitBox2DWidthA;
-            int area2DHitBoxB = hitBox2DLengthB * hitBox2DWidthB;
-
-            // 3D Hit Box Measurements 
-            int hitBox3DLengthA = 0; 
-            int hitBox3DWidthA = 0; 
-            int hitBox3DHeightA = 0; 
-            int hitBox3DLengthB = 0; 
-            int hitBox3DWidthB = 0; 
-            int hitBox3DHeightB = 0; 
+            // Hit Box A Measurements 
+            int hitBoxALength = 0; 
+            int hitBoxAWidth = 0; 
+            int hitBoxAHeight = 0; 
             
+            
+            // Hit Box B Measurements 
+            int hitBoxBLength = 0; 
+            int hitBoxBWidth = 0; 
+            int hitBoxBHeight = 0; 
+
             // 2D Hit Box Calculations
-            int area3DHitBoxA = hitBox3DLengthA * hitBox3DWidthA * hitBox3DHeightA;
-            int area3DHitBoxB = hitBox3DLengthB * hitBox3DWidthB * hitBox3DHeightB;
+            int area2DHitBoxA = hitBoxALength * hitBoxAWidth;
+            int area2DHitBoxB = hitBoxBLength * hitBoxBWidth;
+                    
+            // 3D Hit Box Calculations
+            int area3DHitBoxA = hitBoxALength * hitBoxAWidth * hitBoxAHeight;
+            int area3DHitBoxB = hitBoxBLength * hitBoxBWidth * hitBoxBHeight;
+
 
         }
     }
