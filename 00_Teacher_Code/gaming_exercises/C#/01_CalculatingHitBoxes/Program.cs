6a942f81bfa5573a5bddf2c0625cd20e1df997c7
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-04 12:27:07 -0400

    01_CalculatingHitBoxes Project Creation

---
+++ 00_Teacher_Code/gaming_exercises/C#/01_CalculatingHitBoxes/Program.cs
@@ -0,0 +1,13 @@
+﻿// Calculating Hit Boxes, Ryan Kelley, 11/04/22, 12:26PM, v0.0 
+using System;
+
+namespace _01_CalculatingHitBoxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
