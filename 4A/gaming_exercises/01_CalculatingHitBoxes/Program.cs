9cd21d0ff621f5262dc64fe53a1441d0d2a343f6
Kelley <kelleyr@duvalschools.org>
2022-11-04 14:03:40 -0400

    v0.1
    Declared and defined hit box variables.

--- 4A/gaming_exercises/01_CalculatingHitBoxes/Program.cs
+++ 4A/gaming_exercises/01_CalculatingHitBoxes/Program.cs
@@ -1,4 +1,4 @@
-﻿// Calculating Hit Boxes, Ryan Kelley, 11/04/22, 1:53PM, v0.0
+﻿// Calculating Hit Boxes, Ryan Kelley, 11/04/22, 1:53PM, v0.1
 using System;
 
 namespace _01_CalculatingHitBoxes
@@ -7,7 +7,22 @@ namespace _01_CalculatingHitBoxes
     {
         static void Main(string[] args)
         {
-            
+            int hitBoxType = 0; // 2D = 1, 3D = 2
+
+            // Hit Box A Measurements 
+            int boxALength = 0; 
+            int boxAWidth = 0; 
+            int boxAHeight = 0; 
+
+            // Hit Box B Measurements 
+            int boxBLength = 0; 
+            int boxBWidth = 0; 
+            int boxBHeight = 0; 
+
+
+
+
+
 
 
         }
