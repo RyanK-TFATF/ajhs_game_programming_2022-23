d1ff39a0980a383542307442a21f2d9b4aa3874b
Kelley <kelleyr@duvalschools.org>
2022-11-07 12:20:40 -0500

    v0.2
    Calculate 2D and 3D area for each hit box.

--- 7B/gaming_exercises/01_calculating_hitboxes/calculatingHitBoxes.py
+++ 7B/gaming_exercises/01_calculating_hitboxes/calculatingHitBoxes.py
@@ -1,4 +1,4 @@
-# Calc. Hit Boxes, Ryan Kelley, v0.1 7B 
+# Calc. Hit Boxes, Ryan Kelley, v0.2 7B 
 
 # Hit Box Type
 hitBoxType = 0 # 2D Box = 1, 3D Box = 2 
@@ -12,3 +12,12 @@ boxAHeight = 0
 boxBLength = 0 
 boxBWidth = 0 
 boxBHeight = 0 
+
+# 2D Hit Box Areas 
+boxA2DArea = boxALength * boxAWidth
+boxB2DArea = boxBLength * boxBWidth
+
+# 3D Hit Box Area 
+boxA3DArea = boxALength * boxAWidth * boxAHeight
+boxB3DArea = boxBLength * boxBWidth * boxBHeight
+
