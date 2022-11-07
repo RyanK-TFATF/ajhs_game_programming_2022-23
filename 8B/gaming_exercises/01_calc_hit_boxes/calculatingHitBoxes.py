d9e370e48a98e7aba6dd23c206d86f119475cf3a
Kelley <kelleyr@duvalschools.org>
2022-11-07 14:15:09 -0500

    8B v0.1
    Declare and define box measurements.

--- 8B/gaming_exercises/01_calc_hit_boxes/calculatingHitBoxes.py
+++ 8B/gaming_exercises/01_calc_hit_boxes/calculatingHitBoxes.py
@@ -1,1 +1,15 @@
-# Calc. Hit Boxes, ryan kelley, v0.0 11-07-22 13:57 
+# Calc. Hit Boxes, ryan kelley, v0.1 11-07-22 13:57 
+
+# Hit Box Type 
+boxType = 0 # 1 = 2D Box, 2 = 3D Box 
+
+# Box A
+boxALength = 0 
+boxAWidth = 0 
+boxAHeight = 0 
+
+# Box B
+boxBLength = 0 
+boxBWidth = 0 
+boxBHeight = 0 
+
