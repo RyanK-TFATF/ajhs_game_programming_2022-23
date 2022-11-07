0a31d053119e170f4f729e8b27a4832086f23b40
Kelley <kelleyr@duvalschools.org>
2022-11-07 12:09:15 -0500

    7B v0.1
    Declared and defined hit box measurements for box A and box B.

---
+++ 7B/gaming_exercises/01_calculating_hitboxes/calculatingHitBoxes.py
@@ -0,0 +1,14 @@
+# Calc. Hit Boxes, Ryan Kelley, v0.1 7B 
+
+# Hit Box Type
+hitBoxType = 0 # 2D Box = 1, 3D Box = 2 
+
+# Hit Box A Measurements 
+boxALength = 0 
+boxAWidth = 0 
+boxAHeight = 0 
+
+# Hit Box B Measurements 
+boxBLength = 0 
+boxBWidth = 0 
+boxBHeight = 0 
