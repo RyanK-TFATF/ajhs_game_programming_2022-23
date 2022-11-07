1d8e3b4aadf7c7e7bd275eb8be075499127663d3
Kelley <kelleyr@duvalschools.org>
2022-11-07 09:34:34 -0500

    5B v0.1
    Declared and defined variables for hit box measurements.

--- 5B/gaming_exercises/01_CalculatingHitBoxes/Program.cs
+++ 5B/gaming_exercises/01_CalculatingHitBoxes/Program.cs
@@ -1,4 +1,4 @@
-﻿// Calc. Hit Boxes, Ryan KElley, 11/07/22, v0.0 
+﻿// Calc. Hit Boxes, Ryan KElley, 11/07/22, v0.1 
 using System;
 
 namespace _01_CalculatingHitBoxes
@@ -7,7 +7,22 @@ namespace _01_CalculatingHitBoxes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declaring Variables 
+            // 2D or 3D Hit Box? 
+            int hitBoxType = 0; // 1 = 2D hit box, 2 = 3D hit box 
+
+            // Hit Box A Measurements 
+            int hitBoxALength = 0; 
+            int hitBoxAWidth = 0; 
+            int hitBoxAHeight = 0; 
+
+            // Hit Box B Measurements 
+            int hitBoxBLength = 0; 
+            int hitBoxBWidth = 0; 
+            int hitBoxBHeight = 0;
+
+
+
         }
     }
 }
