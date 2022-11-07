af2982c69e5d3998143a22a87f03b73f5b2b8871
Kelley <kelleyr@duvalschools.org>
2022-11-07 12:44:04 -0500

    v0.4
    Input boxType

--- 7B/gaming_exercises/01_calculating_hitboxes/calculatingHitBoxes.py
+++ 7B/gaming_exercises/01_calculating_hitboxes/calculatingHitBoxes.py
@@ -1,7 +1,7 @@
-# Calc. Hit Boxes, Ryan Kelley, v0.3 7B 
+# Calc. Hit Boxes, Ryan Kelley, v0.4 7B 
 
 # Hit Box Type
-hitBoxType = 0 # 2D Box = 1, 3D Box = 2 
+boxType = 0 # 2D Box = 1, 3D Box = 2 
 
 # Hit Box A Measurements 
 boxALength = 0 
@@ -30,3 +30,9 @@ boxBLength = int(input("Please enter the LENGTH of box B.\n"))
 boxBWidth = int(input("Please enter the WIDTH of box B.\n")) 
 boxBHeight = int(input("Please enter the HEIGHT of box B.\n")) 
 
+# Verify Measurements 
+print(f"Box A Length: {boxALength} Box A Width: {boxAWidth} Box A Height: {boxAHeight}\n")
+print(f"Box B Length: {boxBLength} Box B Width: {boxBWidth} Box B Height: {boxBHeight}\n")
+
+# Input Hit Box Type Selection 
+boxType = int(input("What type of hit box do you want to calculate?  1 = 2D hit box. 2 = 3D hit box.\n")) 
