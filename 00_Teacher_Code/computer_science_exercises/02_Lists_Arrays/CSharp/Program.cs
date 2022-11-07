f73fe1b6a53d9a3e884178efc8eb6442ea1dccdf
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-06 23:37:24 -0500

    v0.1
    Declare and define three different types of arrays.

--- 00_Teacher_Code/computer_science_exercises/02_Lists_Arrays/CSharp/Program.cs
+++ 00_Teacher_Code/computer_science_exercises/02_Lists_Arrays/CSharp/Program.cs
@@ -7,7 +7,24 @@ namespace ListsArrays
     {
         static void Main(string[] args)
         {
+            // Declaring Arrays -- Method 0 -- Creating Empty Array
+            // dataType[] arrayName;
+            // Values can be added later. 
+
+            // Declaring Arrays -- Method 1 -- Creating Array with Values
+            // dataType[] arrayName = {value0, value1, value2, ...};
+            // Values can be changed later. 
+
+            // Declaring Arrays -- Method 2 -- Create Array and Specify Size 
+            // dataType[] arrayName = new dataType[numElements];
+
+            // Declare and Define Three Arrays -- string, integer, and float or double v0.1
+            string[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
+            int[] testScores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
             
+            
+
         }
     }
 }
