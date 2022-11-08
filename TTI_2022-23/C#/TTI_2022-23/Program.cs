3546d02687e685a5db4878f736d620257dad2e3c
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-08 09:48:34 -0500

    Project Creation: TTI 2022-23

---
+++ TTI_2022-23/C#/TTI_2022-23/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TTI_2022_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
