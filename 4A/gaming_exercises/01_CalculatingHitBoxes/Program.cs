bb1328b47dc4fc18e884cbfab4de21181de004da
Kelley <kelleyr@duvalschools.org>
2022-11-08 14:42:39 -0500

    Update Program.cs

--- 4A/gaming_exercises/01_CalculatingHitBoxes/Program.cs
+++ 4A/gaming_exercises/01_CalculatingHitBoxes/Program.cs
@@ -7,8 +7,20 @@ namespace _01_CalculatingHitBoxes
     {
         static void Main(string[] args)
         {
+            // Hit Box Type Selection 
             int hitBoxType = 0; // 2D = 1, 3D = 2
-
+            Console.WriteLine("Please select a hit box type: 1 for 2D, 2 for 3D.");
+            hitBoxType = Convert.ToInt32(Console.ReadLine());
+            if (hitBoxType == 1) {
+                Console.WriteLine("You have chosen a 2D hit box model.\n");    
+            } else if (hitBoxType == 2) {
+                Console.WriteLine("You have chosen a 3D hit box model.\n");    
+            } else {
+                Console.WriteLine("ERROR: Hit Box Type Not Identified!\n");    
+                Console.WriteLine("Please select a hit box type: 1 for 2D, 2 for 3D.");
+                hitBoxType = Convert.ToInt32(Console.ReadLine());
+            }
+            
             // Hit Box A Measurements 
             int boxALength = 0; 
             int boxAWidth = 0; 
@@ -28,9 +40,24 @@ namespace _01_CalculatingHitBoxes
             int boxB2DArea = boxBLength * boxBWidth; 
             int boxB3DArea = boxBLength * boxBWidth * boxBHeight; 
 
+            // Hit Box A Measurments 
+            Console.WriteLine("Please enter the LENGTH of Hit Box A:\n");
+            boxALength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the WIDTH of Hit Box A:\n");
+            boxAWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the HEIGHT of Hit Box A:\n");
+            boxAHeight = Convert.ToInt32(Console.ReadLine());
 
+            // Hit Box B Measurments 
+            Console.WriteLine("Please enter the LENGTH of Hit Box B:\n");
+            boxBLength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the WIDTH of Hit Box B:\n");
+            boxBWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the HEIGHT of Hit Box B:\n");
+            boxBHeight = Convert.ToInt32(Console.ReadLine());
 
-
+            // Verify Measurements
+            Console.WriteLine(String.Format("Box A -- Length: {0} Width: {1} Height: {2}", boxALength, boxAWidth, boxAHeight));
 
 
 
