5dc430d9faa4c74a1e021f9d97062a99e8f79da2
Kelley <kelleyr@duvalschools.org>
2022-11-08 13:17:26 -0500

    v0.4
    Added calculations AFTER inputting values.

--- 3A/gaming_exercises/01_calculating_hit_boxes/calc_hit_box.py
+++ 3A/gaming_exercises/01_calculating_hit_boxes/calc_hit_box.py
@@ -1,4 +1,4 @@
-# Calculating Hit Boxes, Ryan Kelley, 11/04/22, 12:01pm, v0.3
+# Calculating Hit Boxes, Ryan Kelley, 11/04/22, 12:01pm, v0.4
 
 # Declare and Define Variables 
 hitBoxType = 0 # 1 = 2D hitbox, 2 = 3D hitbox
@@ -12,14 +12,6 @@ hitBoxAHeight = 0
 hitBoxBLength = 0
 hitBoxBWidth = 0
 hitBoxBHeight = 0
-
-# Hit Box 2D Area Calculations 
-hitBoxA2DArea = hitBoxALength * hitBoxAWidth
-hitBoxB2DArea = hitBoxBLength * hitBoxBWidth
-
-# Hit Box 3D Area Calculations 
-hitBoxA3DArea = hitBoxALength * hitBoxAWidth * hitBoxAHeight
-hitBoxB3DArea = hitBoxBLength * hitBoxBWidth * hitBoxBHeight
 
 # Select Type of Hit Box 
 hitBoxType = int(input("Which type of hit box do you need to calculate? 1 = 2D or 2 = 3D\n"))
@@ -41,6 +33,14 @@ hitBoxALength = int(input("Please enter an integer value for hit box A length:\n
 hitBoxBAHeight = int(input("Please enter an integer value for hit box B height:\n"))
 hitBoxBWidth = int(input("Please enter an integer value for hit box B width:\n"))
 hitBoxBLength = int(input("Please enter an integer value for hit box B length:\n"))
+
+# Hit Box 2D Area Calculations 
+hitBoxA2DArea = hitBoxALength * hitBoxAWidth
+hitBoxB2DArea = hitBoxBLength * hitBoxBWidth
+
+# Hit Box 3D Area Calculations 
+hitBoxA3DArea = hitBoxALength * hitBoxAWidth * hitBoxAHeight
+hitBoxB3DArea = hitBoxBLength * hitBoxBWidth * hitBoxBHeight
 
 # Verify Measurements
 print(f"Box A -- Length: {hitBoxALength} Height: {hitBoxAHeight} Width: {hitBoxAWidth}\n")
