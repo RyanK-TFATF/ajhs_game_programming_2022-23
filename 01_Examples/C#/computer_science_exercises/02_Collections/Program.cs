46861d3d42f23eba334f130cf38b976cae9298b8
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-08 12:39:59 -0500

    v0.1c
    Demonstrated printing arrays on separate lines.
    Commented out the method we do not want to use.

--- 01_Examples/C#/computer_science_exercises/02_Collections/Program.cs
+++ 01_Examples/C#/computer_science_exercises/02_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 02_Collections, Ryan Kelley, 11-08-2022 v0.1b
+﻿// 02_Collections, Ryan Kelley, 11-08-2022 v0.1c
 using System;
 
 namespace _02_Collections
@@ -32,7 +32,15 @@ namespace _02_Collections
             Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
             Console.WriteLine();
 
-
+            /* Print Array Contents -- Each Element on Separate Line 
+            Console.WriteLine("The elements for each array are:\n"); 
+            Console.WriteLine("breakfastFoods: \n" + String.Join("\n", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join("\n", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join("\n", GPA));
+            Console.WriteLine();
+            */
 
 
 
