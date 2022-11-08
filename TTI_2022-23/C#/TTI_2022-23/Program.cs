c4647e3158728c8bbb7e3a4ecb5bfb680b003ce1
Kelley <kelleyr@duvalschools.org>
2022-11-08 10:19:55 -0500

    TTi Updates

--- TTI_2022-23/C#/TTI_2022-23/Program.cs
+++ TTI_2022-23/C#/TTI_2022-23/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// TTI Practice, Ryan Kelley, 11-08-22 v0.0 
+using System;
 
 namespace TTI_2022_23
 {
@@ -6,7 +7,17 @@ namespace TTI_2022_23
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Data Types 
+            int myInt = 5; // INTEGERS -- Whole Numbers, +/- and 0 
+            float myFloat = 3.12525f; // 7 decimals, +/- and 0.0 
+            double myDouble = 1.999999999999999; // 16 decimals, +/- and 0.0
+            string myString = "Put a string."; // think "russian" 
+            bool myBool = true; // true or false 
+
+            Console.WriteLine(myString); 
+            Console.Write(myInt); 
+            Console.Write(myFloat); 
+
         }
     }
 }
---
+++ TTI_2022-23/Python/TTI.py
@@ -0,0 +1,10 @@
+# TTI Practice, Ryan Kelley, 11-08-22 v0.0 
+
+# Data Types 
+myInt = 5 # INTEGERS -- Whole Numbers, +/- and 0 
+myFloat = 1.25 # FLOATS -- Numbers with decimal, +/- and 0.0 
+myString = "Put a string.\n" # STRINGS - "think russian"
+myBool = True # BOOLEAN -- True or False
+
+print(myString)
+print(my) 
