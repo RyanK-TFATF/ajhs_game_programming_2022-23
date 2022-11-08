2b3baad297e70306b376287482cd188e366c5624
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-08 11:03:26 -0500

    Major Updates for 02_Collections C#

--- 00_Teacher_Code/computer_science_exercises/02_Collections/CSharp/Program.cs
+++ 00_Teacher_Code/computer_science_exercises/02_Collections/CSharp/Program.cs
@@ -6,34 +6,101 @@ namespace _02_Collections
     {
         static void Main(string[] args)
         {
-            // Declaring Arrays -- Method 0 -- Creating Empty Array
-            // dataType[] arrayName;
-            // Values can be added later. 
+            
+            /* Arrays 
+            -- Collection of multiple values under one variable.
+            -- Number of elements CANNOT change. 
+            -- Conents of elements CAN change. 
+            -- Element position in the array is called the INDEX. 
+            -- Array indexes start at 0. 
+            */ 
+                    
+            /* Declaring Arrays 
+            Method 1 -- Creating Array with Values
+            Other methods exist but only this method will be used in class. 
+            */             
 
-            // Declaring Arrays -- Method 1 -- Creating Array with Values
-            // dataType[] arrayName = {value0, value1, value2, ...};
-            // Values can be changed later. 
-
-            // Declaring Arrays -- Method 2 -- Create Array and Specify Size 
-            // dataType[] arrayName = new dataType[numElements];
-
-            // Declare and Define Three Arrays -- string, integer, and float or double v0.1
+            // Declare and Define Three Arrays -- string, integer, and float or double v0.1a
             string[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
             int[] testScores = {95, 100, 25, 15, 27, 35};
             float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
+
+            // Print Array Contents -- All Elements on Single Line v0.1b 
+            Console.WriteLine("The contents for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join(", ", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
+            Console.WriteLine();
+
+            // Print Array Contents -- One Elements Per Line v0.1c
+            Console.WriteLine("The contents for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join("\n", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join("\n", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join("\n", GPA));
+            Console.WriteLine();
             
-            // Print First Element v0.2a
+            // Determine Array Length v0.1d
+            Console.WriteLine("The length of each array is:");
+            Console.WriteLine("breakFastFoods: " + breakfastFoods.Length);
+            Console.WriteLine("testScores: " + testScores.Length);
+            Console.WriteLine("GPA: " + GPA.Length);
+            
+            // Access First Element v0.2a
+            Console.WriteLine("The first element in each array is:\n");
             Console.WriteLine(breakfastFoods[0]);
             Console.WriteLine(testScores[0]);
             Console.WriteLine(GPA[0]);
 
-            // Print First Element v0.2b
-            
+            // Access Last Element v0.2b
+            Console.WriteLine("The last element in each array is: \n");
+            Console.WriteLine(breakfastFoods[breakfastFoods.Length - 1]);
+            Console.WriteLine(testScores[testScores.Length -1]);
+            Console.WriteLine(GPA[GPA.Length -1]);
 
-            // Print Array Length v0.3 
-            Console.WriteLine(breakfastFoods.Length);
-            Console.WriteLine(testScores.Length);
-            Console.WriteLine(GPA.Length);
+            // Access 3rd Element v0.2c -- PWYOC
+            Console.WriteLine("The third element in each array is:\n");
+            Console.WriteLine(breakfastFoods[2]);
+            Console.WriteLine(testScores[2]);
+            Console.WriteLine(GPA[2]);
+
+            // Change Elements in Array v0.3a 
+            breakfastFoods[0] = "Fried Squid";
+            testScores[0] = 59; 
+            GPA[0] = 1.34f; 
+            Console.WriteLine("The contents for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join(", ", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
+            Console.WriteLine();
+
+            // Change Elements in Array 0.3b -- PWYOC
+            breakfastFoods[4] = "Greasy Grimy Gopher Guts";
+            testScores[4] = 1; 
+            GPA[4] = 0.02f; 
+            Console.WriteLine("The contents for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScores: \n" + String.Join(", ", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
+            Console.WriteLine();            
+
+            // Common Array Bugs -- 0.4a 
+            // Index Out of Bounds -- Accessing an element that does not exist. 
+            // Console.WriteLine(breakFastFoods[5]); 
+
+            // Incorrect Data Types 
+            // testScores[0] = "Billy";
+
+
+
+            
         }
     }
 }
