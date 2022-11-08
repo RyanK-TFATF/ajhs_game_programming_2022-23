f6f3d536e39dbfd99b18468105b2f8ee7b0abb6d
Kelley <kelleyr@duvalschools.org>
2022-11-08 08:57:46 -0500

    1A v0.1
    Declared and defined measurements for box A and B.

--- 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
+++ 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
@@ -1,1 +1,17 @@
-# Calc. Hit Boxes, ryan kelley, 11-08-22 v0.0
+# Calc. Hit Boxes, ryan kelley, 11-08-22 v0.1
+
+# Hit Box Type 
+hitBoxType = 0 # 2D Box = 1, 3D Box = 2 
+
+# Hit Box A Measurements 
+boxALength = 0
+boxAWidth = 0
+boxAHeight = 0
+
+# Hit Box B Measurements 
+boxBLength = 0
+boxBWidth = 0
+boxBHeight = 0
+
+
+
