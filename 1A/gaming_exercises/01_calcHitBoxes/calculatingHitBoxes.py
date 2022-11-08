537d96942ac5e319dcf2dcc72e0071eea40c601f
Kelley <kelleyr@duvalschools.org>
2022-11-08 09:05:05 -0500

    1A v0.2
    Declare and define box area calculations for A and B.

--- 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
+++ 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
@@ -1,4 +1,4 @@
-# Calc. Hit Boxes, ryan kelley, 11-08-22 v0.1
+# Calc. Hit Boxes, ryan kelley, 11-08-22 v0.2
 
 # Hit Box Type 
 hitBoxType = 0 # 2D Box = 1, 3D Box = 2 
@@ -13,5 +13,11 @@ boxBLength = 0
 boxBWidth = 0
 boxBHeight = 0
 
+# Box A Area Calculations 
+boxA2DArea = boxALength * boxAWidth
+boxA3DArea = boxALength * boxAWidth * boxAHeight
+# Box B Area Calculations 
+boxB2DArea = boxBLength * boxBWidth
+boxB3DArea = boxBLength * boxBWidth * boxBHeight
 
 
