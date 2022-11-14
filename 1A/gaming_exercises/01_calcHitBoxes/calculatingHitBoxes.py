e073ca462c187401fa3141d1de02e701836b8482
Kelley <kelleyr@duvalschools.org>
2022-11-14 09:39:44 -0500

    v0.what

--- 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
+++ 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
@@ -2,7 +2,15 @@
 
 # Hit Box Type 
 hitBoxType = 0 # 2D Box = 1, 3D Box = 2 
-
+# Hit Box Choice Selection 
+hitBoxType = int(input("Select the hit box type: 1 = 2D, 2 = 3D.\n"))
+if hitBoxType == 1:
+    print("2D Hit Box Model Selected. ")
+elif hitBoxType == 2:
+    print("3D Hit Box Model Selected. ")
+else:
+    print("ERROR: INCORRECT HIT BOX MODEL SELECTED.\n")
+    hitBoxType = int(input("Select the hit box type: 1 = 2D, 2 = 3D.\n"))
 
 # Hit Box A Measurements 
 boxALength = 0
@@ -14,16 +22,6 @@ boxBLength = 0
 boxBWidth = 0
 boxBHeight = 0
 
-# Box A Area Calculations 
-boxA2DArea = boxALength * boxAWidth
-boxA3DArea = boxALength * boxAWidth * boxAHeight
-# Box B Area Calculations 
-boxB2DArea = boxBLength * boxBWidth
-boxB3DArea = boxBLength * boxBWidth * boxBHeight
-
-# Hit Box Choice Selection 
-hitBoxType = int(input("Select the hit box type: 1 = 2D, 2 = 3D.\n"))
-
 # Box A Measurements Input 
 boxALength = int(input("Please enter the LENGTH for Box A.\n"))
 boxAWidth = int(input("Please enter the WIDTH for Box A.\n"))
@@ -33,6 +31,27 @@ boxBLength = int(input("Please enter the LENGTH for Box B.\n"))
 boxBWidth = int(input("Please enter the WIDTH for Box B.\n"))
 boxBHeight = int(input("Please enter the HEIGHT for Box B.\n"))
 
+# Box A Area Calculations 
+boxA2DArea = boxALength * boxAWidth
+boxA3DArea = boxALength * boxAWidth * boxAHeight
+# Box B Area Calculations 
+boxB2DArea = boxBLength * boxBWidth
+boxB3DArea = boxBLength * boxBWidth * boxBHeight
+
 # Verify Measurements 
 print(f"Box A Measurements -- Length: {boxALength} Width: {boxAWidth} Height: {boxAHeight}\n")
 print(f"Box B Measurements -- Length: {boxBLength} Width: {boxBWidth} Height: {boxBHeight}\n")
+
+# Print Area Calculations 
+if hitBoxType == 1:
+    print(f"Hit Box A 2D Area: {boxA2DArea}")
+    print(f"Hit Box B 2D Area: {boxB2DArea}")
+elif hitBoxType == 2:
+    print(f"Hit Box A 3D Area: {boxA3DArea}")
+    print(f"Hit Box B 3D Area: {boxB3DArea}")
+else:
+    print("Error calculating hit box area. Please restart the program.\n")
+
+# Which box is larger?  If if / elif / else and determine which. 
+
+
