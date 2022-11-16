2d986de09f5dd6b6cb53308a80cee481a5fac10f
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-16 10:45:53 -0500

    v0.2a
    Changing elements in an array.

--- 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
+++ 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Ryan Kelley, v0.1d
+# Collections Examples, Ryan Kelley, v0.2a
 
 # LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES 
 breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
@@ -29,5 +29,22 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 #print(testScores[2])
 #print(classGPA[2])
 
+# Changing Items in a List 
+#breakfastFoods[0] = "Sausage"
+#testScores[0] = 97
+#classGPA[0] = 3.57 
+#print(breakfastFoods[0])
+#print(testScores[0])
+#print(classGPA[0])
+#print(breakfastFoods)
+#print(testScores)
+#print(classGPA)
 
+# Pause -- WYOC  -- Change 5th Element
+breakfastFoods[4] = "Bagel"
+testScores[4] = 45
+classGPA[4] = 2.45
+print(breakfastFoods)
+print(testScores)
+print(classGPA)
 
