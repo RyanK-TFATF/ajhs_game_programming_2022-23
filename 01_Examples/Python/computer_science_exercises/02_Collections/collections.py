0bd2cc498768390642ccb65c8781d9922c4711e7
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-16 10:22:57 -0500

    v0.1b
    Printing List Contents

--- 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
+++ 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Ryan Kelley, v0.1a
+# Collections Examples, Ryan Kelley, v0.1b
 
 # LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES 
 breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
@@ -8,5 +8,11 @@ breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
 # Python Only: index -1 it is the last item on the list.  
 testScores = [95, 100, 25, 15, 27, 35]
 classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+# Printing Contents of an List 
+print(breakfastFoods)
+print(testScores)
+print(classGPA)
+
 
 
