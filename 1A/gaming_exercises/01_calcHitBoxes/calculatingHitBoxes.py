b8599b2cf2bc505a347dc646f9d663766f5a894c
Kelley <kelleyr@duvalschools.org>
2022-11-16 09:47:34 -0500

    Update calculatingHitBoxes.py

--- 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
+++ 1A/gaming_exercises/01_calcHitBoxes/calculatingHitBoxes.py
@@ -33,10 +33,10 @@ boxBHeight = int(input("Please enter the HEIGHT for Box B.\n"))
 
 # Box A Area Calculations 
 boxA2DArea = boxALength * boxAWidth
-boxA3DArea = boxALength * boxAWidth * boxAHeight
+boxA3DVol = boxALength * boxAWidth * boxAHeight
 # Box B Area Calculations 
 boxB2DArea = boxBLength * boxBWidth
-boxB3DArea = boxBLength * boxBWidth * boxBHeight
+boxB3DVol = boxBLength * boxBWidth * boxBHeight
 
 # Verify Measurements 
 print(f"Box A Measurements -- Length: {boxALength} Width: {boxAWidth} Height: {boxAHeight}\n")
@@ -47,11 +47,37 @@ if hitBoxType == 1:
     print(f"Hit Box A 2D Area: {boxA2DArea}")
     print(f"Hit Box B 2D Area: {boxB2DArea}")
 elif hitBoxType == 2:
-    print(f"Hit Box A 3D Area: {boxA3DArea}")
-    print(f"Hit Box B 3D Area: {boxB3DArea}")
+    print(f"Hit Box A Volume: {boxA3DVol}")
+    print(f"Hit Box B Volume: {boxB3DVol}")
 else:
-    print("Error calculating hit box area. Please restart the program.\n")
+    print("Error calculating hit box. Please restart the program.\n")
 
 # Which box is larger?  If if / elif / else and determine which. 
+if hitBoxType == 1 and boxA2DArea > boxB2DArea:
+    print("Box A 2D area is greater than Box B 2D area.") 
+    # Calculate the Difference Between Hit Boxes 
+    boxDiff = boxA2DArea - boxB2DArea
+    # Calculate the Average of the Box Areas 
+    avgArea = (boxA2DArea + boxB2DArea) / 2 
+    # Divide Average by Difference 
+    diffDivByAvg = boxDiff / avgArea
+    percentDiff = diffDivByAvg * 100 
+    print(f"Box A is {percentDiff:.2f}% larger than Box B.")
 
+elif hitBoxType == 1 and boxB2DArea > boxA2DArea:
+    print("Box B 2D area is greater than Box A 2D area.") 
+
+elif hitBoxType == 1 and boxB2DArea == boxA2DArea:
+    print("Box B 2D area is equal to Box A 2D area.") 
+
+elif hitBoxType == 2 and boxA3DVol > boxB3DVol:
+    print("Box A volume is greater than Box B volume.") 
+
+elif hitBoxType == 2 and boxB3DVol > boxA3DVol:
+    print("Box B volume is greater than Box A volume.") 
+
+elif hitBoxType == 2 and boxB3DVol == boxA3DVol:
+    print("Box B volume is equal to Box A volume.") 
+else: 
+    print("ERROR: Unable to complete calculations. Please restart and try again.")
 
