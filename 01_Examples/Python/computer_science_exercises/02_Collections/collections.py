3fd282d221533a51810f67e5c53e6f0ec92974c5
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-16 10:56:11 -0500

    v0.2b
    Using .insert() and .append() to add items to lists.

--- 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
+++ 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
@@ -1,4 +1,4 @@
-# Collections Examples, Ryan Kelley, v0.2a
+# Collections Examples, Ryan Kelley, v0.2b
 
 # LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES 
 breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
@@ -41,10 +41,34 @@ classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
 #print(classGPA)
 
 # Pause -- WYOC  -- Change 5th Element
-breakfastFoods[4] = "Bagel"
-testScores[4] = 45
-classGPA[4] = 2.45
-print(breakfastFoods)
-print(testScores)
-print(classGPA)
+#breakfastFoods[4] = "Bagel"
+#testScores[4] = 45
+#classGPA[4] = 2.45
+#print(breakfastFoods)
+#print(testScores)
+#print(classGPA)
 
+# Adding and Inserting Items to a List 
+# .append() adds an item to the END of a list. 
+#breakfastFoods.append("hash browns")
+#print(breakfastFoods)
+#testScores.append(99)
+#print(testScores)
+#classGPA.append(1.99)
+#print(classGPA)
+
+# .insert() allows you to place an item at a specific index in the list. 
+#breakfastFoods.insert(3, "Parfait")
+#print(breakfastFoods)
+#testScores.insert(3, 55)
+#print(testScores)
+#classGPA.insert(3, 0.0)
+#print(classGPA)
+
+# PAUSE -- WYOC -- .append() another item to each list.  .insert() an item at index 5 to each list. 
+breakfastFoods.append("Honey Bun")
+print(breakfastFoods)
+testScores.append(100)
+print(testScores)
+classGPA.append(4.0)
+print(classGPA)
