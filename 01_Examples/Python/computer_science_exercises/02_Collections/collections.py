37b64d9a0535476a05bd1ed1e8cad844e109b2de
Ryan Kelley <kelleyr@duvalschools.org>
2022-11-16 10:18:19 -0500

    v0.1a
    Declared and defined three lists.

--- 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
+++ 01_Examples/Python/computer_science_exercises/02_Collections/collections.py
@@ -1,1 +1,12 @@
-# Collections Examples, Ryan Kelley, v0.0 
+# Collections Examples, Ryan Kelley, v0.1a
+
+# LIST -- ORDERED, CHANGEABLE, ALLOWS DUPLICATE VALUES 
+breakfastFoods = ["Bacon", "Waffles", "Pancakes", "Cereal", "Milk"]
+# Each item on the list is known as an ELEMENT.  
+# The position in the list for each is the INDEX. 
+# The element "BACON" is at index 0.  
+# Python Only: index -1 it is the last item on the list.  
+testScores = [95, 100, 25, 15, 27, 35]
+classGPA = [3.14, 2.25, 1.74, 1.99, 0.99, 4.25]
+
+
