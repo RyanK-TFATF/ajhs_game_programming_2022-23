c3c0816ce7e731798c02638d5a38de7b21cebeac
Kelley <kelleyr@duvalschools.org>
2023-01-04 11:46:42 -0500

    3A -- Loops -- v0.1a
    Basic while loop.

--- 3A/computer_science_exercises/03_Loops/loops.py
+++ 3A/computer_science_exercises/03_Loops/loops.py
@@ -1,1 +1,10 @@
-# Loops, Ryan Kelley, v0.0 
+# Loops, Ryan Kelley, v0.1a 
+
+# while loop -- Best used when the number of loops needed is NOT KNOWN 
+loopCounter = 0 
+while loopCounter < 100: 
+    print(f"This is iteration number {loopCounter}.")
+    loopCounter += 1 # INCREMENT the loop counter. 
+
+
+
