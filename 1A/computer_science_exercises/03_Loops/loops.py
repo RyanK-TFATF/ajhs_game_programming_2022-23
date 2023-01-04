c419e582102b635e2dd5c91fe5503c663a6a4d00
Kelley <kelleyr@duvalschools.org>
2023-01-04 08:58:07 -0500

    Loops v0.1a

--- 1A/computer_science_exercises/03_Loops/loops.py
+++ 1A/computer_science_exercises/03_Loops/loops.py
@@ -1,2 +1,22 @@
-# Loops, Ryan Kelley, v0.0 
+# Loops, Ryan Kelley, v0.1a 
+
+# Two Major Types: while loop and for loop 
+# while loop - use when you DO NOT know how many times the loop will run
+# for loop - use when you DO know how many times the loop will run 
+
+# While Loops -- Musical Chairs 
+
+loopCounter = 0 # YOU ALWAYS NEED A VARIABLE TO TRACK # OF LOOPS (ITERATIONS)
+while loopCounter < 100: # CONDITIONAL STATEMENT TO START LOOP 
+    print(f"This is iteration number {loopCounter}.")
+    print(f"There are {100 - loopCounter} iterations remaining.")
+    loopCounter += 1
+print("The loop is now finished.")   
+
+
+
+
+
+
+    
 
