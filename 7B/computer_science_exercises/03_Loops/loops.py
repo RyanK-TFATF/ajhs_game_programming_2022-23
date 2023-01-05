659dfcdd0f91e8c3d9e628eaa14ca63b20a3f9c6
Kelley <kelleyr@duvalschools.org>
2023-01-05 12:50:16 -0500

    7B -- Loops -- v0.2e

--- 7B/computer_science_exercises/03_Loops/loops.py
+++ 7B/computer_science_exercises/03_Loops/loops.py
@@ -1,8 +1,50 @@
-# 03_Loops, Ryan Kelley, v0.1b
-
+# 03_Loops, Ryan Kelley, v0.2a
+import random
 # while loop -- 
+'''
 loopCounter = 0 
 while loopCounter < 500: # CONDITIONAL STATEMENT -- If TRUE, run loop.
     print(f"This loop has performed {loopCounter} iterations.")
-    loopCounter += 1 
+    # Exiting Loops with break 
+    if loopCounter == 450:
+        break
+    loopCounter +=  random.randint(1, 6) 
+print("END OF FIRST LOOP")
+loopCounterTwo = 500
+while loopCounterTwo > 0:
+    print(f"Loop Counter: {loopCounterTwo}")
+    loopCounterTwo += -1
+'''
+
+# for loops 
+animals = ["cat", "platypus", "velociraptor", "moose", "squirrel"] 
+colors = ["red", "orange", "yellow", "green", "blue"]
+
+'''
+for eachAnimal in animals: 
+    print(eachAnimal)
+    if eachAnimal == "velociraptor":
+        print("Clever girl.")
+    else:
+        print("Aww, so cute.")
+for eachColor in colors: 
+    print(eachColor)
+'''
+# Nested For Loops 
+# Outer Loop Starts Here
+outerLoop = 0
+innerLoop = 0
+
+for x in colors: 
+    print(f"Outer Loop Count: {outerLoop}")
+    outerLoop += 1
+    for y in animals: 
+        print(x, y)
+        innerLoop += 1
+        print(f"Inner Loop Counter: {innerLoop}")
+
+
+
+
+
 
