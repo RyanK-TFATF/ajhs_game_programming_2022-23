82484acd4ef1831e69ee8fa14fbaaf69eaae479f
Kelley <kelleyr@duvalschools.org>
2023-01-05 11:29:42 -0500

    5B -- Loops -- v0.2a

--- 5B/computer_science_exercises/03_Loops/Program.cs
+++ 5B/computer_science_exercises/03_Loops/Program.cs
@@ -1,4 +1,4 @@
-﻿// 03_Loops, Ryan Kelley, v0.1c
+﻿// 03_Loops, Ryan Kelley, v0.2a
 using System;
 
 namespace _03_Loops
@@ -7,7 +7,7 @@ namespace _03_Loops
     {
         static void Main(string[] args)
         {
-           // while loop -- best used when you do not know number of iterations
+           /* while loop -- best used when you do not know number of iterations
            int loopCounter = 0; 
            while (loopCounter < 1000)
            {
@@ -19,15 +19,43 @@ namespace _03_Loops
             }
             Console.WriteLine("Loop Counter:" + loopCounter); 
             loopCounter++; 
-            /* Break from loop. 
+             Break from loop. 
             if (loopCounter == 750)
             {
                 break; // STOP THE LOOP! 
             }
             */ 
             
+            /* do/while loop 
+            int i = 0; 
+            do 
+            {
+                Console.WriteLine("Loop Counter:" + i); 
+                i++; 
+            }
+            while (i < 1);
+            do block ALWAYS RUNS ONE TIME, then runs based on while condition. 
+            */
 
-           }
+            // for loops 
+            // STATEMENT 1 -- executes ONCE before the loop starts. 
+            // STATEMENT 2 -- CONDITIONAL statement, checked each time after 1st loop
+            // STATEMENT 3 -- executes EVERY TIME after loop completes 
+            // for (statement1; statement2; statement3)
+            for (int i = 0; i <= 250; i++)
+            {
+                Console.WriteLine(i);
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("This number is even.");
+                } else {
+                    Console.WriteLine("This number is odd."); 
+                }
+            }
+
+            
+
+           
 
 
         }
