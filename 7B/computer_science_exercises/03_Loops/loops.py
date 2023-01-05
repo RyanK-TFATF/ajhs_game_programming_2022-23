8123c3eda78d4fd49b54b529275efac3a12f73e0
Kelley <kelleyr@duvalschools.org>
2023-01-05 11:56:09 -0500

    7B -- Loops -- v0.1b
    while loop example

---
+++ 7B/computer_science_exercises/03_Loops/loops.py
@@ -0,0 +1,8 @@
+# 03_Loops, Ryan Kelley, v0.1b
+
+# while loop -- 
+loopCounter = 0 
+while loopCounter < 500: # CONDITIONAL STATEMENT -- If TRUE, run loop.
+    print(f"This loop has performed {loopCounter} iterations.")
+    loopCounter += 1 
+
