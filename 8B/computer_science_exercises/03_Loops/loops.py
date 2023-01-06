203589775d060ccf5826e93f85185072c6722ce2
Kelley <kelleyr@duvalschools.org>
2023-01-06 08:11:26 -0500

    8B -- Loops -- v0.3a

--- 8B/computer_science_exercises/03_Loops/loops.py
+++ 8B/computer_science_exercises/03_Loops/loops.py
@@ -1,7 +1,53 @@
-# 03_Loops, Ryan Kelley, v0.1a 
-
+# 03_Loops, Ryan Kelley, v0.3a 
+import random 
+import time 
 # while loop 
-loopCounter = 0 
+'''
+loopCounter = 100 
+while loopCounter >= 0:
+    print(f"Loop Counter: {loopCounter}")
+    loopCounter += -1 
 while loopCounter < 100: 
     print(f"Loop Counter: {loopCounter}")
-    loopCounter += 1
+    if loopCounter == 75:
+        break
+    loopCounter += random.randint(1, 6)
+''' 
+
+# for loops 
+colors = ["red", "orange", "yellow", "green", "blue"]
+animals = ["platypus", "kookaburra", "velociraptor", "moose", "squirrel"] 
+'''
+for eachColor in colors: 
+    print(eachColor)
+    if eachColor == "green":
+        print("I am so envious!")
+for x in animals:
+    print(x)
+    if x == "velociraptor":
+        print("Clever girl.")
+'''
+
+'''
+# Nested Loops -- 
+outerLoop = 0
+innerLoop = 0
+# Outer Loop 
+for x in colors:
+    print(f"Outer Loop Count: {outerLoop}")
+    outerLoop += 1
+    # Inner Loop
+    time.sleep(1)
+    for y in animals: 
+        print(x, y) 
+        print(f"Inner Loop: {innerLoop}")
+        innerLoop +=1 
+'''
+for y in range(10):
+    print(y)
+
+for y in range(50, 100):
+    print(y)
+
+for y in range(1, 10000, 25):
+    print(y)
