61ecdbcee6ae37a29babd82a8c061065ab938c4e
Kelley <kelleyr@duvalschools.org>
2023-01-06 14:10:08 -0500

    4A -- Inventory -- Create Inventory

--- 4A/gaming_exercises/03_Inventory/Program.cs
+++ 4A/gaming_exercises/03_Inventory/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿// 03_Inventory, Ryan Kelley, v0.0 
+using System;
 
 namespace _03_Inventory
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var playerInventory = new ArrayList()
+            {
+                "blue health potion",
+                "green mana potion",
+                "bronze sword",
+                "bronze shield",
+                "steel helmet",
+                "steel armor",
+                "torch",
+                "turkey leg",
+                "beef jerky", 
+                "lantern"
+            }; 
+
         }
     }
 }
