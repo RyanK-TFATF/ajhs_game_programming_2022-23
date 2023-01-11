b4d05f75752a64cafc2e8da1e0134f36d9def797
Kelley <kelleyr@duvalschools.org>
2023-01-11 11:37:35 -0500

    7B -- Inventory -- v0.1a

---
+++ 7B/gaming_exercises/03_Inventory/inventory.py
@@ -0,0 +1,17 @@
+# 03_Inventory, Ryan Kelley, v0.1a 
+
+p1inventory = [
+    "iron sword", 
+    "steel helmet", 
+    "moldy bread", 
+    "canteen", 
+    "blue key"
+]
+
+p2inventory = [
+    "iron axe", 
+    "steel shield", 
+    "moldy cheese", 
+    "cup of water", 
+    "green key"
+]
