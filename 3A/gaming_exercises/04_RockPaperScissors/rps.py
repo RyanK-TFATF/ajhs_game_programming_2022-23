cc58e2542b3b16efdbf7d6fb37c4a7d8b4165b68
Kelley <kelleyr@duvalschools.org>
2023-01-12 11:58:14 -0500

    3A -- RPS -- v0.2a

--- 3A/gaming_exercises/04_RockPaperScissors/rps.py
+++ 3A/gaming_exercises/04_RockPaperScissors/rps.py
@@ -1,4 +1,5 @@
-# 04_RockPaperScissors, Ryan Kelley, v0.1
+# 04_RockPaperScissors, Ryan Kelley, v0.2
+import random 
 
 # Declare Variables 
 playerChoice = ""
@@ -12,4 +13,34 @@ choices = [
     "paper", 
     "scissors"
 ]
+
+# Game Functions 
+def displayInstructions(): 
+    print("*==========================================*")
+    print("*                Welcome to                *")
+    print("*    Ryan's Rock -- Paper -- Scissors      *")
+    print("*                                          *")
+    print("* To play, you will choose Rock, Paper, or *")
+    print("* Scissors.                                *")
+    print("*                                          *")
+    print("* Rules:                                   *")
+    print("* Rock Beats Scissors                      *")
+    print("* Paper Beats Rock                         *")
+    print("* Scissors Beats Paper                     *")
+    print("*                                          *")
+    print("* Winner of each round gets 1 point.       *")
+    print("* First to score 3 points wins the match!  *")
+    print("*==========================================*")
+
+#displayInstructions()
+
+# Make CPU Selection 
+def cpuChoice(): 
+    return choices[random.randint(0, 2)]
+
+# x = 0 
+# while x <= 50: 
+#     print(cpuChoice())
+#     x += 1
+
 
