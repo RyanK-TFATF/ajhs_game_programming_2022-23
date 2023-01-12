31f4b8ed700f679265e18db3cf2a764639d8746c
Kelley <kelleyr@duvalschools.org>
2023-01-12 11:32:22 -0500

    3A -- RPS -- v0.1a

---
+++ 3A/gaming_exercises/04_RockPaperScissors/rps.py
@@ -0,0 +1,15 @@
+# 04_RockPaperScissors, Ryan Kelley, v0.1
+
+# Declare Variables 
+playerChoice = ""
+playerScore = 0
+
+cpuChoice = ""
+cpuScore = 0 
+
+choices = [
+    "rock", 
+    "paper", 
+    "scissors"
+]
+
