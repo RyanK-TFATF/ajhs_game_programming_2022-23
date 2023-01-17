64dcfb98d7a14cb3b7eb6eda05a425bc27a6149b
Kelley <kelleyr@duvalschools.org>
2023-01-17 12:03:54 -0500

    3A -- RPS -- v0.5a
    Added draws = 0
    
    Updated xChoice() to xRPS() function name.
    
    Added code to modify inputs for each function.

--- 01_A/3A/gaming_exercises/04_RockPaperScissors/rps.py
+++ 01_A/3A/gaming_exercises/04_RockPaperScissors/rps.py
@@ -1,4 +1,4 @@
-# 04_RockPaperScissors, Ryan Kelley, v0.2
+# 04_RockPaperScissors, Ryan Kelley, v0.5a
 import random 
 
 # Declare Variables 
@@ -7,6 +7,8 @@ playerScore = 0
 
 cpuChoice = ""
 cpuScore = 0 
+
+draws = 0 
 
 choices = [
     "rock", 
@@ -35,56 +37,76 @@ def displayInstructions():
 #displayInstructions()
 
 # Make CPU Selection 
-def cpuChoice(): 
+def cpuRPS(): 
     return choices[random.randint(0, 2)]
 
-cpuChoice = cpuChoice()
+cpuChoice = cpuRPS()
 
 # Player Choice 
-def playerChoice(): 
+def playerRPS(): 
     print("Ok, let's get down to business and play rock, paper, scissors!")
     playerChoice = input("Please type Rock, Paper, or Scissors and press [ENTER].\n")
+    playerChoice = playerChoice[0].lower()    
+    if playerChoice == "r":
+        playerChoice = "Rock"
+    elif playerChoice == "s":
+        playerChoice = "Scissors"
+    elif playerChoice == "p":
+        playerChoice = "Paper"
+    else:
+        print("Error Choosing Rock, Paper, Scissors.")
     print(f"You have chosen {playerChoice}.\n")
+
     while True: 
         isCorrect = input("Is this correct? Yes / No\n")
-        if isCorrect == "Yes":
+        isCorrect = isCorrect[0].lower()
+        if isCorrect == "y":
             break
         else: 
             playerChoice = input("Please type Rock, Paper, or Scissors and press [ENTER].\n")
+            if playerChoice == "r":
+                playerChoice = "Rock"
+            elif playerChoice == "s":
+                playerChoice = "Scissors"
+            elif playerChoice == "p":
+                playerChoice = "Paper"
+            else:
+                print("Error Choosing Rock, Paper, Scissors.")
     return playerChoice
 
-#playerChoice = cpuChoice()
+#playerRPS()
 
 def roundWinner(playerChoice, cpuChoice):
     print(f"You have chosen {playerChoice}.\n")
     print(f"The CPU has chosen {cpuChoice}.\n")
     if playerChoice == "Rock" and cpuChoice == "Rock": 
         print("That is a draw.  Womp womp.\n")
+        roundWinner = "draw"
     elif playerChoice == "Rock" and cpuChoice == "Paper": 
         print("The CPU wins, you scrub.\n")
+        roundWinner = "cpu"
     elif playerChoice == "Rock" and cpuChoice == "Scissors": 
-        print("You win! Much wow.\n")
+        print("You win! Much wow.\n")        
+        roundWinner = "player"
     elif playerChoice == "Paper" and cpuChoice == "Rock": 
         print("You win! Much wow.\n")
+        roundWinner = "player"
     elif playerChoice == "Paper" and cpuChoice == "Paper": 
         print("That is a draw.  Womp womp.\n")
+        roundWinner = "draw"
     elif playerChoice == "Paper" and cpuChoice == "Scissors": 
         print("The CPU wins, you scrub.\n")
+        roundWinner = "cpu"
     elif playerChoice == "Scissors" and cpuChoice == "Rock": 
         print("The CPU wins, you scrub.\n")
+        roundWinner = "cpu"
     elif playerChoice == "Scissors" and cpuChoice == "Paper": 
         print("You win! Much wow.\n")
+        roundWinner = "player"
     elif playerChoice == "Scissors" and cpuChoice == "Scissors": 
         print("That is a draw.  Womp womp.\n")
+        roundWinner = "draw"
     else:
         print("Unable to determine a winner.  Please restart.") 
         exit()
-
-x = 0 
-while x <= 500: 
-   playerChoice = cpuChoice()
-   cpuChoice = cpuChoice()
-   roundWinner(playerChoice, cpuChoice)
-   x += 1
-
-
+    return roundWinner 
