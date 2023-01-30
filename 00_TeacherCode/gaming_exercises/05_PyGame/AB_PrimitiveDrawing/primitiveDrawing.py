508683445ed545ce119554f2f4e08aff003a0247
Ryan Kelley <kelleyr@duvalschools.org>
2023-01-30 10:53:47 -0500

    Example -- PyGame: Primitive Drawing -- 0.4
    Setup the PyGame requirements for new project.

---
+++ 00_TeacherCode/gaming_exercises/05_PyGame/AB_PrimitiveDrawing/primitiveDrawing.py
@@ -0,0 +1,9 @@
+# Primitive Drawing, Ryan Kelley, v0.4
+import pygame, sys
+from pygame.locals import *
+
+pygame.init() 
+
+# Setup Window 
+DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption('Primitive Drawing')
