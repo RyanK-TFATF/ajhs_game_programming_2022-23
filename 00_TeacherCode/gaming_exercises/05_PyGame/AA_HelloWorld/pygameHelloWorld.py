0cdfef92a779f410c249de4b945302c6b85ee3b3
Ryan Kelley <kelleyr@duvalschools.org>
2023-01-30 10:29:06 -0500

    Example -- PyGame -- v0.2
    Imported pygame and sys modules.
    Initialized the pygame module with pygame.init()
    Created DISPLAYSURF and set the window caption.

--- 00_TeacherCode/gaming_exercises/05_PyGame/AA_HelloWorld/pygameHelloWorld.py
+++ 00_TeacherCode/gaming_exercises/05_PyGame/AA_HelloWorld/pygameHelloWorld.py
@@ -1,1 +1,9 @@
-# Hello World with PyGame, Ryan Kelley, v0.1
+# Hello World with PyGame, Ryan Kelley, v0.2
+
+import pygame, sys 
+from pygame.locals import * 
+
+pygame.init() # .init() is short for INITIALIZE.  
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello, World!')
+
