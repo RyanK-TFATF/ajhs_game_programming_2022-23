e3bd86f17666cb8f03fa2dcbd7f6cbce6591f291
Ryan Kelley <kelleyr@duvalschools.org>
2023-01-30 10:35:39 -0500

    Example: PyGame -- v0.3
    Created our game loop to display a blank window with the caption, 'Hello, World!'

--- 00_TeacherCode/gaming_exercises/05_PyGame/AA_HelloWorld/pygameHelloWorld.py
+++ 00_TeacherCode/gaming_exercises/05_PyGame/AA_HelloWorld/pygameHelloWorld.py
@@ -1,4 +1,4 @@
-# Hello World with PyGame, Ryan Kelley, v0.2
+# Hello World with PyGame, Ryan Kelley, v0.3
 
 import pygame, sys 
 from pygame.locals import * 
@@ -6,4 +6,13 @@ from pygame.locals import *
 pygame.init() # .init() is short for INITIALIZE.  
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello, World!')
+
+while True:
+    for event in pygame.event.get(): 
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update() 
+
+
 
