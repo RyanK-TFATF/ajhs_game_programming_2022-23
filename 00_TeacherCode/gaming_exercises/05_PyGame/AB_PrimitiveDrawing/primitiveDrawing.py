214fd5937813822cd6f10e35d90301e4d8dd40e8
Ryan Kelley <kelleyr@duvalschools.org>
2023-01-30 11:08:10 -0500

    PyGame -- Primitive Drawing -- v0.6
    Draw primitive shapes using pygame.draw

--- 00_TeacherCode/gaming_exercises/05_PyGame/AB_PrimitiveDrawing/primitiveDrawing.py
+++ 00_TeacherCode/gaming_exercises/05_PyGame/AB_PrimitiveDrawing/primitiveDrawing.py
@@ -1,4 +1,4 @@
-# Primitive Drawing, Ryan Kelley, v0.5
+# Primitive Drawing, Ryan Kelley, v0.6
 import pygame, sys
 from pygame.locals import *
 
@@ -15,5 +15,16 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Draw on the Surface Object 
+DISPLAYSURF.fill(WHITE)
+pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
+pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
+pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
+pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
+pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
+pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
+
 
 
