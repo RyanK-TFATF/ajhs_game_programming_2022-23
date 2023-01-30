59ff6aaa3b2dbc21bc9c183c4813d95588712e73
Ryan Kelley <kelleyr@duvalschools.org>
2023-01-30 10:58:03 -0500

    Example -- Primitive Drawing -- v0.5
    Setup the color values for black, white, green, red, and blue.

--- 00_TeacherCode/gaming_exercises/05_PyGame/AB_PrimitiveDrawing/primitiveDrawing.py
+++ 00_TeacherCode/gaming_exercises/05_PyGame/AB_PrimitiveDrawing/primitiveDrawing.py
@@ -1,4 +1,4 @@
-# Primitive Drawing, Ryan Kelley, v0.4
+# Primitive Drawing, Ryan Kelley, v0.5
 import pygame, sys
 from pygame.locals import *
 
@@ -7,3 +7,13 @@ pygame.init()
 # Setup Window 
 DISPLAYSURF = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption('Primitive Drawing')
+
+# Color Setup 
+# COLORNAME = (RedValue, GreenValue, BlueValue) 0 = No Value, 255 = Max Value 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
