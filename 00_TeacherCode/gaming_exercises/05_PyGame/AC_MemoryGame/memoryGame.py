f86f5790741cade699a93ff67ff44fb228e14d31
Ryan Kelley <kelleyr@duvalschools.org>
2023-02-01 10:59:05 -0500

    Memory Game -- v0.9 -- Color Values

--- 00_TeacherCode/gaming_exercises/05_PyGame/AC_MemoryGame/memoryGame.py
+++ 00_TeacherCode/gaming_exercises/05_PyGame/AC_MemoryGame/memoryGame.py
@@ -1,4 +1,4 @@
-# Memory Game, Ryan Kelley, v0.8 -- based on a project by Al Sweigart.  
+# Memory Game, Ryan Kelley, v0.9 -- based on a project by Al Sweigart.  
 
 import pygame, sys, random
 from pygame.locals import *
@@ -15,3 +15,20 @@ BOARDHEIGHT = 7 # Number of Rows in Game Board
 assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board must be an even number of boxes for matched pairs.'
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
+
+# Colors 
+GRAY = (100, 100, 100)
+NAVYBLUE = (60, 60, 100)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 128, 0)
+PURPLE = (255, 0, 255)
+CYAN = (0, 255, 255)
+
+BGCOLOR = NAVYBLUE
+LIGHTBGCOLOR = GRAY
+BOXCOLOR = WHITE
+HIGHLIGHTCOLOR = BLUE
