eadf4ff4515165b2327e562efd06ddec359aa4cd
Ryan Kelley <kelleyr@duvalschools.org>
2023-02-22 14:42:39 -0500

    Update lootBoxCalculator.py

--- 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
+++ 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
@@ -1,1 +1,34 @@
 # Loot Box Cost Calculator, Ryan Kelley, v0.0 
+
+def createCommonItems(num):
+    # Test
+    itemCount = 0 
+    commonItems = []
+    while len(commonItems) < num:
+        commonItems.append("Common Item " + str(itemCount))
+        itemCount += 1
+    print(commonItems)
+    return commonItems 
+
+# createCommonItems(5)
+
+def createUncommonItems(num):
+    # Test
+    itemCount = 0 
+    commonItems = []
+    while len(commonItems) < num:
+        commonItems.append("Common Item " + str(itemCount))
+        itemCount += 1
+    print(commonItems)
+    return commonItems
+
+def createRareItems(num):
+    # Test
+    itemCount = 0 
+    commonItems = []
+    while len(commonItems) < num:
+        commonItems.append("Common Item " + str(itemCount))
+        itemCount += 1
+    print(commonItems)
+    return commonItems
+
