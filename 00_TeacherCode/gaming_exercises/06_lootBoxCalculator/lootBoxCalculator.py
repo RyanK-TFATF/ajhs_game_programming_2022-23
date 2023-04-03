70aa4bf75cb33de07fbc090a29d9dab0f53fafc4
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-03 11:40:09 -0400

    Update lootBoxCalculator.py

--- 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
+++ 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
@@ -10,30 +10,31 @@ def main():
     # Common Items
     numCommon = int(input("How many common items are available in your game?\n"))
     commonChance = float(input("What is the percentage chance of getting a common item in the loot box?  Enter as a decimal.\n"))    
-    commonItemsAvailable = createCommonItems(numCommon)
-    commonItemsOpened = []
+    print(commonChance)
+    # commonItemsAvailable = createCommonItems(numCommon)
+    # commonItemsOpened = []
 
-    # Uncommon Items
-    numUncommon = int(input("How many uncommon items are available in your game?\n"))
-    uncommonChance = float(input("What is the percentage chance of getting an uncommon item in the loot box?  Enter as a decimal.\n"))
-    uncommonItems = createUncommonItems(numUncommon)
-    uncommonItemsOpened = []
+    # # Uncommon Items
+    # numUncommon = int(input("How many uncommon items are available in your game?\n"))
+    # uncommonChance = float(input("What is the percentage chance of getting an uncommon item in the loot box?  Enter as a decimal.\n"))
+    # uncommonItems = createUncommonItems(numUncommon)
+    # uncommonItemsOpened = []
     
-    # Rare Items
-    numRare = int(input("How many rare items are available in your game?\n"))
-    rareChance = float(input("What is the percentage chance of getting a rare item in the loot box?  Enter as a decimal.\n"))
-    rareItems = createRareItems(numRare)
-    rareItemsOpened = []
+    # # Rare Items
+    # numRare = int(input("How many rare items are available in your game?\n"))
+    # rareChance = float(input("What is the percentage chance of getting a rare item in the loot box?  Enter as a decimal.\n"))
+    # rareItems = createRareItems(numRare)
+    # rareItemsOpened = []
     
-    # Loot Box Structure
-    numItemsPerBox = int(input("How many items are in each box?\n"))
-    rareGuaranteed = -1
-    while rareGuaranteed != 0 or rareGuaranteed != 1:
-        int(input("Are you guaranteed at least one rare item per loot box? Enter 0 for No, 1 for yes.\n"))
-        if rareGuaranteed == 0:
-            rareGuaranteed = False
-        elif rareGuaranteed == 1:
-            rareGuaranteed = True
+    # # Loot Box Structure
+    # numItemsPerBox = int(input("How many items are in each box?\n"))
+    # rareGuaranteed = -1
+    # while rareGuaranteed != 0 or rareGuaranteed != 1:
+    #     int(input("Are you guaranteed at least one rare item per loot box? Enter 0 for No, 1 for yes.\n"))
+    #     if rareGuaranteed == 0:
+    #         rareGuaranteed = False
+    #     elif rareGuaranteed == 1:
+    #         rareGuaranteed = True
 
     
     
