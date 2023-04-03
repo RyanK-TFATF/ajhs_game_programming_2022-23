9f01794b655278cb5e57dda77e8b227ba5c8b56c
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-03 11:11:25 -0400

    v0.1 -- Updated Item Generation Functions

--- 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
+++ 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
@@ -1,11 +1,23 @@
-# Loot Box Cost Calculator, Ryan Kelley, v0.0 
+# Loot Box Cost Calculator, Ryan Kelley, v0.1 
+
+def main(): 
+    numCommon = int(input("How many common items are available in your game?\n"))
+    numUncommon = int(input("How many uncommon items are available in your game?\n"))
+    numRare = int(input("How many rare items are available in your game?\n"))
+
+    commonItems = createCommonItems(numCommon)
+    uncommonItems = createUncommonItems(numUncommon)
+    rareItems = createRareItems(numRare)
+    print(commonItems)
+    print(uncommonItems)
+    print(rareItems)
 
 def createCommonItems(num):
     # Test
     itemCount = 0 
     commonItems = []
     while len(commonItems) < num:
-        commonItems.append("Common Item " + str(itemCount))
+        commonItems.append("Common Item #" + str(itemCount))
         itemCount += 1
     print(commonItems)
     return commonItems 
@@ -15,20 +27,22 @@ def createCommonItems(num):
 def createUncommonItems(num):
     # Test
     itemCount = 0 
-    commonItems = []
-    while len(commonItems) < num:
-        commonItems.append("Common Item " + str(itemCount))
+    uncommonItems = []
+    while len(uncommonItems) < num:
+        uncommonItems.append("Uncommon Item #" + str(itemCount))
         itemCount += 1
-    print(commonItems)
-    return commonItems
+    print(uncommonItems)
+    return uncommonItems
 
 def createRareItems(num):
     # Test
     itemCount = 0 
-    commonItems = []
-    while len(commonItems) < num:
-        commonItems.append("Common Item " + str(itemCount))
+    rareItems = []
+    while len(rareItems) < num:
+        rareItems.append("Rare Item #" + str(itemCount))
         itemCount += 1
-    print(commonItems)
-    return commonItems
+    print(rareItems)
+    return rareItems
 
+
+main() 
