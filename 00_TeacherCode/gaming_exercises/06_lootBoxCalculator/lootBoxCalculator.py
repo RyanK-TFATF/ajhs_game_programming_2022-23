e9cd695d5ee5b9148f2c13f90f9296c7473a7005
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-03 11:54:38 -0400

    Update lootBoxCalculator.py

--- 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
+++ 00_TeacherCode/gaming_exercises/06_lootBoxCalculator/lootBoxCalculator.py
@@ -1,51 +1,49 @@
 # Loot Box Cost Calculator, Ryan Kelley, v0.2 
-# import randint 
+from random import randint
 # import sys, os for file access
 # open file to save lootbox info. 
 
 def main(): 
-    percentage = 100 
+    
     craftingMaterials = 0 
 
     # Common Items
     numCommon = int(input("How many common items are available in your game?\n"))
-    commonChance = float(input("What is the percentage chance of getting a common item in the loot box?  Enter as a decimal.\n"))    
-    print(commonChance)
-    # commonItemsAvailable = createCommonItems(numCommon)
-    # commonItemsOpened = []
+    commonChance = int(input("What is the percentage chance of getting a common item in the loot box?  Enter an integer number without a % sign..\n"))    
+    
+    commonItemsAvailable = createCommonItems(numCommon)
+    commonItemsOpened = []
 
-    # # Uncommon Items
-    # numUncommon = int(input("How many uncommon items are available in your game?\n"))
-    # uncommonChance = float(input("What is the percentage chance of getting an uncommon item in the loot box?  Enter as a decimal.\n"))
-    # uncommonItems = createUncommonItems(numUncommon)
-    # uncommonItemsOpened = []
+    # Uncommon Items
+    numUncommon = int(input("How many uncommon items are available in your game?\n"))
+    uncommonChance = float(input("What is the percentage chance of getting an uncommon item in the loot box?  Enter as a decimal.\n"))
+    uncommonItems = createUncommonItems(numUncommon)
+    uncommonItemsOpened = []
     
-    # # Rare Items
-    # numRare = int(input("How many rare items are available in your game?\n"))
-    # rareChance = float(input("What is the percentage chance of getting a rare item in the loot box?  Enter as a decimal.\n"))
-    # rareItems = createRareItems(numRare)
-    # rareItemsOpened = []
+    # Rare Items
+    numRare = int(input("How many rare items are available in your game?\n"))
+    rareChance = float(input("What is the percentage chance of getting a rare item in the loot box?  Enter as a decimal.\n"))
+    rareItems = createRareItems(numRare)
+    rareItemsOpened = []
     
-    # # Loot Box Structure
-    # numItemsPerBox = int(input("How many items are in each box?\n"))
-    # rareGuaranteed = -1
-    # while rareGuaranteed != 0 or rareGuaranteed != 1:
-    #     int(input("Are you guaranteed at least one rare item per loot box? Enter 0 for No, 1 for yes.\n"))
-    #     if rareGuaranteed == 0:
-    #         rareGuaranteed = False
-    #     elif rareGuaranteed == 1:
-    #         rareGuaranteed = True
+    # Are you guaranteed at least one rare item per loot box? 
+    rareGuaranteed = int(input("Are you guaranteed at least one rare item per loot box? Enter 0 for No, 1 for yes.\n"))
+    if rareGuaranteed != 0 or rareGuaranteed != 1:        
+        rareGuaranteed = int(input("Are you guaranteed at least one rare item per loot box? Enter 0 for No, 1 for yes.\n"))
+    if rareGuaranteed == 0:
+        rareGuaranteed = False        
+    elif rareGuaranteed == 1:
+        rareGuaranteed = True
+    
+    # Loot Box Structure
+    numItemsPerBox = int(input("How many items are in each box?\n"))           
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    if randint(1,100) <= commonChance: 
+        commonItemsOpened.append(commonItemsAvailable[randint(0, commonItemsAvailable.len())])
+        print(commonItemsOpened)
 
+
+# Common Item Functions 
 def createCommonItems(num):
     # Test
     itemCount = 0 
@@ -56,6 +54,7 @@ def createCommonItems(num):
     print(commonItems)
     return commonItems 
 
+# Unommon Item Functions 
 def createUncommonItems(num):
     # Test
     itemCount = 0 
@@ -66,6 +65,7 @@ def createUncommonItems(num):
     print(uncommonItems)
     return uncommonItems
 
+# Rare Item Functions 
 def createRareItems(num):
     # Test
     itemCount = 0 
