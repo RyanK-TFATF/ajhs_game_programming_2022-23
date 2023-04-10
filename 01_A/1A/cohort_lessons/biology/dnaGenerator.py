be53bddc1d87523d23fe554ecf5cc414920e43b8
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-10 08:58:54 -0400

    Update dnaGenerator.py

--- 01_A/1A/cohort_lessons/biology/dnaGenerator.py
+++ 01_A/1A/cohort_lessons/biology/dnaGenerator.py
@@ -4,3 +4,23 @@
 from random import randint 
 # from the random module, import ONLY the randint function.
 
+dnaBases = ["A", "C", "G", "T"] # Adenine, Cytosine, Guanine, Thymine
+
+# Bases Generated
+basesGenerated = 0 
+
+# Bases Requested
+basesRequested = 0 
+basesRequested = int(input("TYPE A STRING HERE TO ASK FOR NUMBER OF BASES.\n"))
+#print(basesRequested)
+
+# Store the Generated Bases
+dnaSequence = ""
+
+# Generate the Bases
+while basesGenerated < basesRequested: 
+    dnaSequence += dnaBases[randint(0, 3)]
+    basesGenerated += 1
+
+print(dnaSequence)
+
