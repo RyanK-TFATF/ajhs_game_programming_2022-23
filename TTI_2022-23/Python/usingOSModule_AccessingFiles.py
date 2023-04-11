00c7ac9bac8f661cba5aa37417e2dd7aa1c20522
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-11 11:29:09 -0400

    Create usingOSModule_AccessingFiles.py

---
+++ TTI_2022-23/Python/usingOSModule_AccessingFiles.py
@@ -0,0 +1,12 @@
+# import os 
+
+# dirName = "TestDirectory"
+# os.mkdir(dirName)
+
+# import math
+# pi = math.pi 
+# radius = (float(input("Enter a radius for a circle.\n")))
+# area = pi * radius ** 2
+# print(area)
+
+
