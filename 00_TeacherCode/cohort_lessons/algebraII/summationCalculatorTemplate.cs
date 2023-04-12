12c7ea25b1efc10a4d66d86eb78e42c8a01fe422
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-12 14:31:14 -0400

    Update summationCalculatorTemplate.cs

--- 00_TeacherCode/cohort_lessons/algebraII/summationCalculatorTemplate.cs
+++ 00_TeacherCode/cohort_lessons/algebraII/summationCalculatorTemplate.cs
@@ -7,8 +7,18 @@ namespace _01_SummationCalculator
     {
         static void Main(string[] args)
         {
-            
-                            
+            int sum = 0;
+            int lowerLimit = 0;
+            int upperLimit = 0; 
+            Console.WriteLine("What is the lower limit?");
+            lowerLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the upper limit?");
+            upperLimit = Convert.ToInt32(Console.ReadLine());
+            for (int n = lowerLimit; n <= upperLimit; n++)
+            {
+                sum += n * n;
+                Console.WriteLine(sum);
+            }                            
         }
     }
 }
