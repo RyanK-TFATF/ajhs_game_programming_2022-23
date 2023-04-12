08b888e3368a3ce5ee1175b4a267c7f39dbe6a92
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-12 08:18:12 -0400

    Create dnaGenerator.py

---
+++ 02_B/8B/cohort_lessons/biology/dnaGenerator.py
@@ -0,0 +1,80 @@
+# DNA Generator, Ryan Kelley, v0.0 
+
+# Code Execution Timing 
+import time 
+startTime = time.time() 
+x = 0 
+while x < 100000: 
+    x += 1
+
+print(x) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Execution Time %s Seconds" % (time.time() - startTime))
+
+
+
+
+
+
+
+
+
+
+
+
