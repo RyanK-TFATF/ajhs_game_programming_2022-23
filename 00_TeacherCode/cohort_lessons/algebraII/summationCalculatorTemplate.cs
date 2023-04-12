91b2f15067e7bd76a65fa4c83717ddf3e939d1ef
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-12 13:44:39 -0400

    Summations

---
+++ 00_TeacherCode/cohort_lessons/algebraII/summationCalculatorTemplate.cs
@@ -0,0 +1,14 @@
+// Summation Calculator, YOUR_NAME_HERE, v0.0a 
+using System;
+
+namespace _01_SummationCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                            
+        }
+    }
+}
