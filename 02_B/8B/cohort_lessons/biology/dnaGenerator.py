2f0a235f4a938626018292dcdcd4b0190366d500
Ryan Kelley <kelleyr@duvalschools.org>
2023-04-13 14:52:46 -0400

    YOLO -- 8B

--- 02_B/8B/cohort_lessons/biology/dnaGenerator.py
+++ 02_B/8B/cohort_lessons/biology/dnaGenerator.py
@@ -21,15 +21,43 @@ def generateDNASequence():
     # Store the Bases
     dnaSequence = ""
 
-    while basesGenerated <= basesRequested:
+    while basesGenerated < basesRequested:
         dnaSequence += dnaBases[randint(0, 3)]
         basesGenerated += 1 
 
     print(f"DNA Sequence: \n{dnaSequence}\n")    
+    return dnaSequence
 
-generateDNASequence()
+dnaSequence = generateDNASequence()
 
+def rnaTranscription(dnaSequence):
+    rnaSequence = ""
 
+    for eachBase in dnaSequence:
+        if eachBase == "T":
+            rnaSequence += "A"
+        elif eachBase == "A":
+            rnaSequence += "U"
+        elif eachBase == "G":
+            rnaSequence += "C"
+        elif eachBase == "C":
+            rnaSequence += "G"
+        else:
+            print("Transcription Error! Base unknown.\n")
+            exit()
+    print(f"RNA Sequence: \n{rnaSequence}\n")    
+    return rnaSequence
+
+rnaSequence = rnaTranscription(dnaSequence)
+
+saveData = open("dna_rnaData.txt", "a")
+saveData.write(dnaSequence + " " + str(datetime.datetime.now()) + "\n")
+saveData.write(rnaSequence + " " + str(datetime.datetime.now()) + "\n")
+saveData.close()
+# File Modes 
+# "x" -- CREATES FILE, IF FILE EXISTS, EXIT WITH ERROR CODE
+# "a" -- CREATES FILE, IF FILE EXISTS, ADD ON TO END OF FILE
+# "w" -- CREATES FILE, IF FILE EXISTS, OVERWRITE IT 
 
 print("Execution Time %s Seconds" % (time.time() - startTime))
 
