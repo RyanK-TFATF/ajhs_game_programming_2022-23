5e967ca4ef19aad5dad314e91d011e25b36d81e1
Ryan Kelley <kelleyr@duvalschools.org>
2023-05-08 13:05:50 -0400

    3A

---
+++ 01_A/3A/computer_science_exercises/10_cardsDice/cardsDice.py
@@ -0,0 +1,41 @@
+#import random # THIS BRINGS THE WHOLE TOOL BOX. 
+from random import randint # BRING JUST THE TOOL NEEDED
+
+def shuffleDeck(numShuffles):
+    pass # Tells Python our code is not finished, but give no error.
+
+# CALL THE FUNCTION 
+shuffleDeck(7)
+shuffleDeck(500)
+
+def startTimer(numSeconds): 
+    pass
+
+startTimer(120)
+
+def drawCard(numCards):
+    pass
+
+def getValue(card):
+    value = 0 
+    pass
+    return value 
+
+getValue('Jack')
+
+def rollDice(numDice, sizeDice):
+    sum = 0
+    numRolls = 0 
+    while numRolls < numDice:
+        sum += randint(1, sizeDice)
+        numRolls += 1
+    print(f"You rolled {numDice}d{sizeDice} and got a sum of {sum}.")
+    return sum 
+
+
+rollDice(3, 6)
+rollDice(10, 4)
+rollDice(5, 1000)
+rollDice(50000, 4)
+
+
