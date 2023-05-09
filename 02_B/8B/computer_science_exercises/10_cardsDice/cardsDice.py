67f4f20e0d311326efec1ec86b1313b808b8f6e5
Ryan Kelley <kelleyr@duvalschools.org>
2023-05-09 14:43:49 -0400

    Update cardsDice.py

--- 02_B/8B/computer_science_exercises/10_cardsDice/cardsDice.py
+++ 02_B/8B/computer_science_exercises/10_cardsDice/cardsDice.py
@@ -4,13 +4,8 @@ from random import randint # BRING JUST THE TOOL NEEDED
 def shuffleDeck(numShuffle):    
     pass
 
-#shuffleDeck(3)
-
 def startTimer(numSeconds):
     pass
-
-minute = 60
-startTimer(3 * minute)
 
 def drawCard(numCards):
     pass
@@ -28,5 +23,48 @@ def rollDice(numDice, sizeDice):
     print(sum)
     return sum 
 
+def main(): 
+    currentScore = 0 
+    shuffleDeck(5)
+    drawCard(5)
+    yourHand = []
+    for eachCard in yourHand: 
+        value = getValue(card)
+        if value >= 2 or value <= 10:
+            currentScore += value 
+        elif value == 11: # Jack
+            currentScore += rollDice(1, 6)
+        elif value == 12: # Queen
+            currentScore += rollDice(1, 8)
+        elif value == 13: # King 
+            currentScore += rollDice(1, 10)
+        elif value == 14: # Ace
+            currentScore += rollDice(1, 12)
+        else: # UNIDENTIFIED CARD
+            print("Error, unknown card.\n")
+            currentScore = 0
+    
+    startTimer(120)
 
+    while sandInTimer == True: # One grain of sand up top means run the loop!
+        drawCard(1)
+        value = getValue(card)
+        if value >= 2 or value <= 10:
+            currentScore -= value 
+        elif value == 11: # Jack
+            currentScore -= rollDice(1, 6)
+        elif value == 12: # Queen
+            currentScore -= rollDice(1, 8)
+        elif value == 13: # King 
+            currentScore -= rollDice(1, 10)
+        elif value == 14: # Ace
+            currentScore -= rollDice(1, 12)
+        else: # UNIDENTIFIED CARD
+            print("Error, unknown card.\n")
+            currentScore = 0
+
+    if currentScore % 2 == 0:
+        getCandy(10, 10)
+    else: 
+        getCandy(20)
 
