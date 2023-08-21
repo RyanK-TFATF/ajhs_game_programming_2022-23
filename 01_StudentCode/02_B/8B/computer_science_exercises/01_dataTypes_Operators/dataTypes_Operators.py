8c795f05c945df151a6aed0655105536d4e0a621
Ryan K <kelleyr@duvalschools.org>
2023-08-21 14:56:07 -0400

    Update dataTypes_Operators.py

--- 01_StudentCode/02_B/8B/computer_science_exercises/01_dataTypes_Operators/dataTypes_Operators.py
+++ 01_StudentCode/02_B/8B/computer_science_exercises/01_dataTypes_Operators/dataTypes_Operators.py
@@ -1,1 +1,33 @@
 # Data Types and Operators, Ryan Kelley, v0.0 
+
+# Variable Rules 
+# CANNOT START WITH A NUMBER!!!!!!!!!!!!
+# CANNOT USE BUILT-IN KEYWORDS AS VARIABLES.
+# VARIABLE NAME SHOULD DESCRIBE THE DATA BEING STORED. 
+# snake_case variables use _ to separate words, all lower case.
+# camelCase variables do not use spaces, 1st word lower, rest uppper.
+
+# String Literal Examples 
+playerName = "Chad Gamerson"
+emptyString = ""
+spaceString = " "
+monsterName = "Purple People Eater"
+
+# Integer Data Types 
+health = 100
+extraLives = 5 
+temperature = -17 
+
+# Floating Point Data Types 
+pi = 3.1415678
+lapTime = 3.5
+velocity = -2.0 
+
+# Boolean Data Types 
+isFireType = False
+weaponEquipped = True 
+pvpEnabled = False 
+
+
+
+
