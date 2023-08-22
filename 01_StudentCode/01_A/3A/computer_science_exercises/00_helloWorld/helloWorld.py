356a44a826db2b525d075eca3050b9fbc55dc14c
Ryan K <kelleyr@duvalschools.org>
2023-08-22 13:11:31 -0400

    Update helloWorld.py

--- 01_StudentCode/01_A/3A/computer_science_exercises/00_helloWorld/helloWorld.py
+++ 01_StudentCode/01_A/3A/computer_science_exercises/00_helloWorld/helloWorld.py
@@ -11,14 +11,31 @@ print("Hello World!")
 # Variable names CANNOT START WITH NUMBERS
 # Name should describe the data being stored.
 # Cannot use Python keywords as variables. 
+# snake_case -- all words are lower case, separated by _ 
+# camelCase -- first word lower, all other words upper, no spaces. 
+player_health = 100
+playerHealth = 100 
 
-gasRemaining = 10 
-currentTemp = 99.9 
-favoriteFlavor = "Vanilla"
 
-stringVar = "5"
-integerVar = 5
+# gasRemaining = 10 
+# currentTemp = 99.9 
+# favoriteFlavor = "Vanilla"
 
-print(stringVar + stringVar)
-print(integerVar + integerVar)
+# stringVar = "5"
+# integerVar = 5
+
+# print(stringVar + stringVar)
+# print(integerVar + integerVar)
+
+# Variable Examples 
+myInteger = -1
+myFloat = 0.123
+myString = "I like shrimp a la mode."
+myBool = False
+
+# Printing Variables 
+print(myInteger)
+print(myFloat)
+print(myString)
+print(myBool)
 
