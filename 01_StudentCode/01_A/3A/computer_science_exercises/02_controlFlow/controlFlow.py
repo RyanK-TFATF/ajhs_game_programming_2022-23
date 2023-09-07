2ab1c884e82a2639a9880b085b9ae72c184e7dcb
Ryan K <kelleyr@duvalschools.org>
2023-09-07 12:35:31 -0400

    Create controlFlow.py

---
+++ 01_StudentCode/01_A/3A/computer_science_exercises/02_controlFlow/controlFlow.py
@@ -0,0 +1,19 @@
+# Control Flow, ryan Kelley, v0.1 
+
+# DECLARATIONS 
+favColor = "Blue"
+luckyNumber = 25
+myGPA = 2.33
+myAge = 43 
+pineappleOnPizza = True 
+
+# if Statement 
+if luckyNumber > 30:
+    print("Wow, what a great number!")
+
+if favColor != "Green":
+    print("Your favorite color stinks.")
+
+
+
+
