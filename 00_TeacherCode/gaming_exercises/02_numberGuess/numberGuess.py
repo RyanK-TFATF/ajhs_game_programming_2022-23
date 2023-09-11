49ce2e6df348a6a9c03fe99d4167d070c6551094
Ryan Kelley <kelleyr@duvalschools.org>
2023-09-11 11:01:43 -0400

    Update numberGuess.py

--- 00_TeacherCode/gaming_exercises/02_numberGuess/numberGuess.py
+++ 00_TeacherCode/gaming_exercises/02_numberGuess/numberGuess.py
@@ -2,35 +2,37 @@
 import random # What library will allow you to generate random numbers?
 
 numGuesses = 0  # Initialize this variable to 0.
-
+playerGuess = 0 # Initialize this variable to 0. 
 # Use print() to display a STRING introducing the game.
 
 # Use input() to assign this variable a value.
 
 # Use print() to print a STRING containing player_name.
 
-#randNum = random.randint(x, y) # Replace x and y with integers.  X must be less than Y, and Y must be less than 101. 
+secretNum = random.randint(x, y) # Replace x and y with integers.  X must be less than Y, and Y must be less than 101. 
 # random.randint(x, y) returns a random integer between (and including) x and y.
 
 # Use print() to print a STRING with player_name, explain the program will pick a random number from x to y and number of guesses. 
 
-
-for numGuesses in range(): # Replace z with the number of guesses you want the player to receive. 
+while playerScore < 3: 
+    for guesses in range(): # Replace z with the number of guesses you want the player to receive. 
     
     # Use input() to let the player guess a number.  Make sure to convert it using int()!.
 
     # You will create an if/elif/else statement on the next lines to check the number.
-    if True:  
-        pass
+        if True:  
+            pass
         
-    elif False:
-        pass        
-    else:
-        break
-# After z times through the loop, the loop exits and the code continues to exectute the next statements.
+        elif False:
+            pass        
+        else:
+            break
+    # After z times through the loop, the loop exits and the code continues to exectute the next statements.
 
-# if player_guess == rand_num:
-#     num_guess += 1
-#     print()
-# else:
-#     print()
+if playerGuess == secretNum:    
+#   print()
+#playerScore += 1
+   # else:
+    #   print()
+    
+        
