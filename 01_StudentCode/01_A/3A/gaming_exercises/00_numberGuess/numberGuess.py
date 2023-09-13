46ed2fd9d81f46e0cd62e86f7fbd6fe288f8ba2e
Ryan K <kelleyr@duvalschools.org>
2023-09-13 12:03:18 -0400

    Update numberGuess.py

--- 01_StudentCode/01_A/3A/gaming_exercises/00_numberGuess/numberGuess.py
+++ 01_StudentCode/01_A/3A/gaming_exercises/00_numberGuess/numberGuess.py
@@ -1,16 +1,17 @@
 # Pick the secret number from a range for the possible numbers (i.e. 0-10, 0-100, 100-200)
 # Provide the player X number of guesses, based on range of numbers.  
-# Player guesses the number. 
-# Compare guess to secret number. 
-# If the guess is correct what should happen?
-    # Award the player a point. 
-    # Print a 'win' message on the screen. 
-# If the guess is incorrect, what should happen? 
-    # Indicate if guess is high/low compared to secret number. 
-# If the player does not guess correctly before hitting limit, what happens?
-    # Award a point to the CPU. 
-    # Print a loss message.  
 # First player to score at least 3 points is declared the winner. 
+    # Player guesses the number. 
+    # Compare guess to secret number. 
+    # If the guess is correct what should happen?
+        # Award the player a point. 
+        # Print a 'win' message on the screen. 
+    # If the guess is incorrect, what should happen? 
+        # Indicate if guess is high/low compared to secret number. 
+    # If the player does not guess correctly before hitting limit, what happens?
+        # Award a point to the CPU. 
+        # Print a loss message.  
+
 
 # Guess a Number, Ryan Kelley, v0.0 
 import random 
@@ -41,3 +42,27 @@ if isCorrect == "yes":
     print(f"Ok {playerName}, let's continue!")
 else: 
     playerName = input("What should I call you?\nType your name and press enter.\n")    
+
+# GENERATE SECRET NUMBER
+secretNumber = random.randint(0, 20) # INCLUSIVE
+#print(secretNumber)
+
+# PLAYER GUESS 
+print("You need to guess a number from 0 to 20.  You have four guesses!\n")
+      
+while playerScore != 3 or cpuScore != 3: 
+    #pass  Tells Python to skip this block without giving an error. 
+    playerGuess = input("Think of your number, type it in and then push ENTER.\n")
+    print(f"You have picked {playerGuess}.  Let's see if it is a match!\n")
+    if playerGuess == secretNumber: 
+        playerScore += 1 
+        print("A winner is you! It's a match!\n")
+    else: 
+        for guesses in range(4): 
+            if playerGuess < secretNumber: 
+                print("Your guess is too low!\n")
+            else: 
+                print("Your guess is too high!\n")
+
+
+
