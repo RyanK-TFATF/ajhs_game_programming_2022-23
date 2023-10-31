02b8246aa90eea4a437547f4abdbf20b9099a530
Ryan K <kelleyr@duvalschools.org>
2023-10-31 14:51:05 -0400

    Update hangman.py

--- 01_StudentCode/8B/04b_hangman/hangman.py
+++ 01_StudentCode/8B/04b_hangman/hangman.py
@@ -1,7 +1,66 @@
 # Hangman Game by Ryan Kelley, v0.0 
-
+import random 
 words = 'cat bat rat sat mat fat pat apple banana potato tomato pasta fettucine linguine marina mozzarella cheese energy missile plane bazooka phytoplankton mitochondria antidisestablishmentarianism supercalifragilisticexpiealadocius cow pig skunk moose'.split()
 
+# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE! 
+HANGMAN_BOARD = ['''
+    +---+
+        |
+        |
+        |
+     =======''', '''
+    +---+
+    O   |
+        |
+        |
+     =======''', '''
+    +---+
+    O   |
+    |   |
+        |
+     =======''', '''            
+    +---+
+    O   |
+   /|   |
+        |
+     =======''', '''                             
+    +---+
+    O   |
+   /|\  |
+        |
+     =======''', ''' 
+    +---+
+    O   |
+   /|\  |
+   /    |
+     =======''', '''                       
+    +---+
+    O   |
+   /|\  |
+   / \  |
+     =======''']
+
+# Pick Word from List 
+def getRandomWord(wordList): # Return a random word from the list. 
+    wordIndex = random.randint(0, len(wordList) - 1)
+    # len(listName) - 1 is EXTREMELY COMMON FOR WORKING WITH LISTS.  
+    return wordList[wordIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord): 
+    print(HANGMAN_BOARD[len(missedLetters)])
+    print()
+
+    print('Missed Letters:', end = ' ')
+    for eachLetter in missedLetters: 
+        print(eachLetter, end = ' ')
+    print()
+    # FINISH THURSDAY 
+
+# i = 0 
+# while i < 100: 
+#     word = getRandomWord(words)
+#     print(word)
+#     i += 1
 
 
 
