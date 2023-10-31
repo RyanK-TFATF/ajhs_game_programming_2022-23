14f15bc1923d050abe9b1325567f9267abdee7bb
Ryan K <kelleyr@duvalschools.org>
2023-10-31 13:58:04 -0400

    Create hangman.py

---
+++ 01_StudentCode/8B/04b_hangman/hangman.py
@@ -0,0 +1,7 @@
+# Hangman Game by Ryan Kelley, v0.0 
+
+words = 'cat bat rat sat mat fat pat apple banana potato tomato pasta fettucine linguine marina mozzarella cheese energy missile plane bazooka phytoplankton mitochondria antidisestablishmentarianism supercalifragilisticexpiealadocius cow pig skunk moose'.split()
+
+
+
+
