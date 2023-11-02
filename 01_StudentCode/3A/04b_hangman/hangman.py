fd9dd7d078a05cc3a0cc0f36ddf214dad394a784
Ryan K <kelleyr@duvalschools.org>
2023-11-02 08:36:36 -0400

    Test

--- 01_StudentCode/3A/04b_hangman/hangman.py
+++ 01_StudentCode/3A/04b_hangman/hangman.py
@@ -1,2 +1,94 @@
-wordList = ['word1 word2 word3 word4 ... word30'
-]
+import random 
+wordList = 'cat bat rat sat pat fat lunch dinner fettucine steak pasta potato tomato carrot phytoplankton spine skull cardiovascular respiratory integumentary endocrine blue orange green yellow silver purple'.split()
+# .split() will split a string into separate elements, by default based on SPACE
+
+# VARIABLE_NAMES that are ALL CAPS are CONSTANTS and not meant to change. 
+HANGMAN_BOARD = ['''
+    +----+
+         |
+         |
+         |
+         |
+       =====''', '''
+    +----+
+    O    |
+         |
+         |
+         |
+       =====''', ''' 
+    +----+
+    O    |
+    |    |
+         |
+         |
+       =====''', ''' 
+    +----+
+    O    |
+   /|    |
+         |
+         |
+       =====''', ''' 
+    +----+
+    O    |
+   /|\   |
+         |
+         |
+       =====''', ''' 
+    +----+
+    O    |
+   /|\   |
+   /     |
+         |
+       =====''', ''' 
+    +----+
+    O    |
+   /|\   |
+   / \   |
+         |
+       ====='''] 
+
+def getRandomWord(wordList):
+    wordIndex = random.randint(0 , len(wordList) - 1)
+    # len(listName) - 1 IS MOST COMMON WAY TO PREVENT INDEX OUT OF RANGE ERRORS
+    #print(wordIndex)
+    return wordList[wordIndex]
+
+def displayBoard(incorrectLetters, correctLetters, secretWord): 
+    print(HANGMAN_BOARD[len(incorrectLetters)])
+    print(correctLetters) 
+
+                                   
+# i = 0
+# while i < 500000:
+#     getRandomWord(wordList)
+#     i += 1 
+
+
+                
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
+                 
