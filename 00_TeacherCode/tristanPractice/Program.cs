f156396393b4cc169f8bc4bd73bc6e357c132b01
Ryan Kelley <kelleyr@duvalschools.org>
2023-12-18 08:57:35 -0500

    Tristan Test Code

---
+++ 00_TeacherCode/tristanPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tristanPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wins = 17;
+            int losses = 1000; 
+            int[] winsLosses = {1, 2, 3, 4, 5, 6};
+            Console.WriteLine(winsLosses[0]);
+            Console.WriteLine(winsLosses[1]);
+
+        }
+    }
+}
