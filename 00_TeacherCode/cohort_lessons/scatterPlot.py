7010defc4cead3bef1db1d507d6c067832fae531
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2023-12-22 22:18:44 -0500

    Update scatterPlot.py

--- 00_TeacherCode/cohort_lessons/scatterPlot.py
+++ 00_TeacherCode/cohort_lessons/scatterPlot.py
@@ -2,17 +2,15 @@ import matplotlib.pyplot as plot
 import numpy, random
 
 
+def graphType():
+    pass 
+
 def makeHistogram():
     pass
 
 def makeBar():
-    pass
-
-def makeScatter():
     # Number of Data Points
-    print("+=+=+=++=+=+=++=+=+=++=+=+=+=+")
-    print("+=+=+=+ WELCOME MESSAGE +=+=+=+")
-    print("+=+=+=++=+=+=++=+=+=++=+=+")
+    print("+=+=+=+ Bar Graph +=+=+=+\n")    
     print("This program requires an equal number of X and Y data points to plot correctly.\n")
     numPts = int(input("How many data points will you need to plot?  Type an integer value and press enter.\n"))
     count = 0 
@@ -32,10 +30,45 @@ def makeScatter():
     yVals = []
 
     # Data Point Entry
-    print()
-    print("+=+=+=++=+=+=++=+=+=++=+=+")
-    print("+=+=+=+ DATA ENTRY +=+=+=+")
-    print("+=+=+=++=+=+=++=+=+=++=+=+")
+    print()    
+    print("+=+=+=+ DATA ENTRY +=+=+=+\n")    
+    print("\nPlease type the X and Y values for the data points.\nFormat as x, y and then press enter!\n")
+    while count < numPts: 
+        count += 1
+        newVals = input(f"\nType the X and Y values for Data Point {count} and press ENTER.\n").split(',')
+        xVals.append(newVals[0])
+        yVals.append(newVals[1])
+        
+    x = numpy.array(xVals)
+    y = numpy.array(yVals)
+
+    plot.scatter(x, y, marker="*")
+    plot.show()
+
+def makeScatter():
+    # Number of Data Points
+    print("+=+=+=+ Scatter Plot +=+=+=+\n")    
+    print("This program requires an equal number of X and Y data points to plot correctly.\n")
+    numPts = int(input("How many data points will you need to plot?  Type an integer value and press enter.\n"))
+    count = 0 
+
+    # Title 
+    myTitle = input("\nPlease type the TITLE of your scatter plot and press enter.\n")
+    plot.title(myTitle)
+
+    # x Related
+    labelX = input("\nPlease type the X-AXIS LABEL of your scatter plot and press enter.\n")
+    plot.xlabel(labelX)
+    xVals = []
+
+    # y Related
+    labelY = input("\nPlease type the Y-AXIS LABEL of your scatter plot and press enter.\n")
+    plot.ylabel(labelY)
+    yVals = []
+
+    # Data Point Entry
+    print()    
+    print("+=+=+=+ DATA ENTRY +=+=+=+\n")    
     print("\nPlease type the X and Y values for the data points.\nFormat as x, y and then press enter!\n")
     while count < numPts: 
         count += 1
