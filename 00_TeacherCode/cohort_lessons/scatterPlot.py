cb703385552d9df0efc64bf9290671e0b10dcde1
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2023-12-23 12:46:09 -0500

    Update scatterPlot.py

--- 00_TeacherCode/cohort_lessons/scatterPlot.py
+++ 00_TeacherCode/cohort_lessons/scatterPlot.py
@@ -10,42 +10,42 @@ def makeHistogram():
 
 def makeBar():
     # Number of Data Points
-    print("+=+=+=+ Bar Graph +=+=+=+\n")    
-    print("This program requires an equal number of X and Y data points to plot correctly.\n")
-    numPts = int(input("How many data points will you need to plot?  Type an integer value and press enter.\n"))
+    print("+=+=+=+ Bar Graph +=+=+=+\n")        
+    numCats = int(input("How many categories will you need to graph?  Type an integer value and press enter.\n"))
     count = 0 
 
     # Title 
-    myTitle = input("\nPlease type the TITLE of your scatter plot and press enter.\n")
+    myTitle = input("\nPlease type the TITLE of your bar graph and press enter.\n")
     plot.title(myTitle)
 
-    # x Related
-    labelX = input("\nPlease type the X-AXIS LABEL of your scatter plot and press enter.\n")
-    plot.xlabel(labelX)
+    # x Related    
     xVals = []
 
     # y Related
-    labelY = input("\nPlease type the Y-AXIS LABEL of your scatter plot and press enter.\n")
+    labelY = input("\nPlease type the Y-AXIS LABEL of your bar graph and press enter.\n")
     plot.ylabel(labelY)
     yVals = []
 
-    # Data Point Entry
-    print()    
+    # Data Point Entry    
     print("+=+=+=+ DATA ENTRY +=+=+=+\n")    
-    print("\nPlease type the X and Y values for the data points.\nFormat as x, y and then press enter!\n")
-    while count < numPts: 
+    print("\nPlease enter the category name followed by the value for that category.\n")
+    while count < numCats: 
         count += 1
-        newVals = input(f"\nType the X and Y values for Data Point {count} and press ENTER.\n").split(',')
-        xVals.append(newVals[0])
-        yVals.append(newVals[1])
+        catName = input(f"\nEnter the label for Category #{count} and press ENTER.\n")
+        catVal = float(input(f"Enter a numerical value for the {catName} category.  Integers will be converted to decimal numbers.\n"))
+        xVals.append(catName)
+        yVals.append(catVal)
         
     x = numpy.array(xVals)
     y = numpy.array(yVals)
 
-    plot.scatter(x, y, marker="*")
+    plot.bar(x, y)
     plot.show()
 
+makeBar()
+
 def makeScatter():
+
     # Number of Data Points
     print("+=+=+=+ Scatter Plot +=+=+=+\n")    
     print("This program requires an equal number of X and Y data points to plot correctly.\n")
