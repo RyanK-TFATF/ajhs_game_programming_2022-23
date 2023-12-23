dbf502a47de9e788838ef8b0812c229c308334ba
Ryan Kelley <92961892+RyanK-TFATF@users.noreply.github.com>
2023-12-22 22:14:42 -0500

    Refactor to Functions

--- 00_TeacherCode/cohort_lessons/scatterPlot.py
+++ 00_TeacherCode/cohort_lessons/scatterPlot.py
@@ -1,42 +1,50 @@
 import matplotlib.pyplot as plot 
 import numpy, random
 
-# Number of Data Points
-print("+=+=+=++=+=+=++=+=+=++=+=+=+=+")
-print("+=+=+=+ WELCOME MESSAGE +=+=+=+")
-print("+=+=+=++=+=+=++=+=+=++=+=+")
-print("This program requires an equal number of X and Y data points to plot correctly.\n")
-numPts = int(input("How many data points will you need to plot?  Type an integer value and press enter.\n"))
-count = 0 
 
-# Title 
-myTitle = input("\nPlease type the TITLE of your scatter plot and press enter.\n")
-plot.title(myTitle)
+def makeHistogram():
+    pass
 
-# x Related
-labelX = input("\nPlease type the X-AXIS LABEL of your scatter plot and press enter.\n")
-plot.xlabel(labelX)
-xVals = []
+def makeBar():
+    pass
 
-# y Related
-labelY = input("\nPlease type the Y-AXIS LABEL of your scatter plot and press enter.\n")
-plot.ylabel(labelY)
-yVals = []
+def makeScatter():
+    # Number of Data Points
+    print("+=+=+=++=+=+=++=+=+=++=+=+=+=+")
+    print("+=+=+=+ WELCOME MESSAGE +=+=+=+")
+    print("+=+=+=++=+=+=++=+=+=++=+=+")
+    print("This program requires an equal number of X and Y data points to plot correctly.\n")
+    numPts = int(input("How many data points will you need to plot?  Type an integer value and press enter.\n"))
+    count = 0 
 
-# Data Point Entry
-print()
-print("+=+=+=++=+=+=++=+=+=++=+=+")
-print("+=+=+=+ DATA ENTRY +=+=+=+")
-print("+=+=+=++=+=+=++=+=+=++=+=+")
-print("\nPlease type the X and Y values for the data points.\nFormat as x, y and then press enter!\n")
-while count < numPts: 
-    count += 1
-    newVals = input(f"\nType the X and Y values for Data Point {count} and press ENTER.\n").split(',')
-    xVals.append(newVals[0])
-    yVals.append(newVals[1])
-    
-x = numpy.array(xVals)
-y = numpy.array(yVals)
+    # Title 
+    myTitle = input("\nPlease type the TITLE of your scatter plot and press enter.\n")
+    plot.title(myTitle)
 
-plot.scatter(x, y, marker="*")
-plot.show()
+    # x Related
+    labelX = input("\nPlease type the X-AXIS LABEL of your scatter plot and press enter.\n")
+    plot.xlabel(labelX)
+    xVals = []
+
+    # y Related
+    labelY = input("\nPlease type the Y-AXIS LABEL of your scatter plot and press enter.\n")
+    plot.ylabel(labelY)
+    yVals = []
+
+    # Data Point Entry
+    print()
+    print("+=+=+=++=+=+=++=+=+=++=+=+")
+    print("+=+=+=+ DATA ENTRY +=+=+=+")
+    print("+=+=+=++=+=+=++=+=+=++=+=+")
+    print("\nPlease type the X and Y values for the data points.\nFormat as x, y and then press enter!\n")
+    while count < numPts: 
+        count += 1
+        newVals = input(f"\nType the X and Y values for Data Point {count} and press ENTER.\n").split(',')
+        xVals.append(newVals[0])
+        yVals.append(newVals[1])
+        
+    x = numpy.array(xVals)
+    y = numpy.array(yVals)
+
+    plot.scatter(x, y, marker="*")
+    plot.show()
